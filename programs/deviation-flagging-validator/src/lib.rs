f216fac268c785ef9d77532a56f2192952531d55
Blaž Hrastnik <blaz@mxxn.io>
2021-12-08 17:12:51 +0900

    Make it more obvious that these IDs are fake on localnet

--- programs/deviation-flagging-validator/src/lib.rs
+++ programs/deviation-flagging-validator/src/lib.rs
@@ -4,7 +4,7 @@ use arrayvec::arrayvec;
 
 use access_controller::AccessController;
 
-declare_id!("HJmKEmXD9bcCH8UuncvoHD8RVCo7UHSWarRffy1go9X4");
+declare_id!("My11111111111111111111111111111111111111113");
 
 static THRESHOLD_MULTIPLIER: u128 = 100000;
 
