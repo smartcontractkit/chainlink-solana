9825118d33650afec504509a22b3b014003a9192
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2021-12-09 06:20:33 +0000

    add monitoring entrypoint and a short readme

---
+++ cmd/monitoring/README.md
@@ -0,0 +1,48 @@
+# Solana Onchain monitoring
+
+## Help
+
+```
+go run ./cmd/monitoring/*.go -help
+```
+
+## Run locally
+
+Requirements:
+- schema registry
+- kafka
+- solana validator
+- contracts deployed and accounts created
+
+- feeds description in either a file (say /tmp/feeds.json) or an RDD url
+
+Fetch latest master and execute
+```
+go run ./cmd/monitoring/*.go \
+-solana.rpc_endpoint="http://127.0.0.1:8899" \
+-solana.network_name="solana-devnet" \
+-solana.network_id="solana-devnet" \
+-solana.chain_id="1" \
+
+-kafka.topic="solana-devnet" \
+-kafka.brokers="localhost:29029" \
+-kafka.client_id="monitoring" \
+-kafka.security_protocol="PLAINTEST" \
+-kafka.sasl_mechanism="PLAIN" \
+-kafka.sasl_username="" \
+-kafka.sasl_password="" \
+
+-schema_registry.url="localhost:8989" \
+-schema_registry.username="" \
+-schema_registry.password="" \
+
+-feeds.file-path="/tmp/feeds.json" \
+
+-http.address="localhost:3000"
+```
+
+## Build docker image
+
+```
+docker build -f monitoring.Dockerfile -t solana-onchain-monitor:0.1.0 .
+```
---
+++ cmd/monitoring/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gagliardetto/solana-go/rpc"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/smartcontractkit/chainlink-solana/pkg/monitoring"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cfg, err := monitoring.ParseConfig()
+	if err != nil {
+		log.Fatalf("failed to parse configuration: %v", err)
+	}
+
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		if err := http.ListenAndServe(cfg.Http.Address, nil); err != nil {
+			log.Fatalf("failed to start http server with address %s: error %v", cfg.Http.Address, err)
+		}
+	}()
+
+	client := rpc.New(cfg.Solana.RPCEndpoint)
+
+	schemaRegistry := monitoring.NewSchemaRegistry(cfg.SchemaRegistry)
+	trSchema, err := schemaRegistry.EnsureSchema("transmission", monitoring.TransmissionAvroSchema)
+	if err != nil {
+		log.Fatalf("failed to prepare transmission schema with error: %v", err)
+	}
+	stSchema, err := schemaRegistry.EnsureSchema("config_set", monitoring.ConfigSetAvroSchema)
+	if err != nil {
+		log.Fatalf("failed to prepare transmission schema with error: %v", err)
+	}
+
+	producer, err := monitoring.NewProducer(ctx, cfg.Kafka)
+	if err != nil {
+		log.Fatalf("failed to create kafka producer with error: %v", err)
+	}
+
+	trReader := monitoring.NewTransmissionReader(client)
+	stReader := monitoring.NewStateReader(client)
+
+	monitor := monitoring.NewMultiFeedMonitor(
+		cfg.Solana,
+		trReader, stReader,
+		trSchema, stSchema,
+		producer,
+		cfg.Feeds,
+		monitoring.DefaultMetrics,
+	)
+	go monitor.Start(ctx)
+
+	signalsCh := make(chan os.Signal, 1)
+	signal.Notify(signalsCh, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-signalsCh
+	log.Printf("Received signal %v. Stopping\n", sig)
+}
