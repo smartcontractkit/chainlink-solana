87d158fa9367f0d09f7b2c2ea0eeb4ae2eb0e29b
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2021-12-17 17:23:26 +0100

    updated program ids

--- contracts/programs/deviation-flagging-validator/src/lib.rs
+++ contracts/programs/deviation-flagging-validator/src/lib.rs
@@ -4,7 +4,7 @@ use arrayvec::arrayvec;
 
 use access_controller::AccessController;
 
-declare_id!("My11111111111111111111111111111111111111113");
+declare_id!("EnUkr8ruCbCe1EidYghGMYanjz6ifXAxphWZmLMri9Zg");
 
 static THRESHOLD_MULTIPLIER: u128 = 100000;
 
