3fe44c532b7d838288dcf3c5cd93c1093b0389c4
aalu1418 <aalu1418@gmail.com>
2022-01-14 10:47:10 -0500

    small improvements to return answer data and make logging optional

--- pkg/solana/utils.go
+++ pkg/solana/utils.go
@@ -3,6 +3,8 @@ package solana
 import (
 	"context"
 	"fmt"
+	"math/big"
+	"time"
 
 	"github.com/gagliardetto/solana-go"
 	"github.com/pkg/errors"
@@ -11,7 +13,7 @@ import (
 )
 
 // XXXInspectStates prints out state data, it should only be used for inspection
-func XXXInspectStates(state, transmission, program, rpc string) error {
+func XXXInspectStates(state, transmission, program, rpc string, log bool) (answer *big.Int, timestamp time.Time, err error) {
 	tracker := ContractTracker{
 		StateID:         solana.MustPublicKeyFromBase58(state),
 		TransmissionsID: solana.MustPublicKeyFromBase58(transmission),
@@ -27,51 +29,54 @@ func XXXInspectStates(state, transmission, program, rpc string) error {
 
 	cfg, err := tracker.LatestConfig(context.TODO(), 0)
 	if err != nil {
-		return errors.Wrap(err, "error in tracker.LatestConfig")
+		return answer, timestamp, errors.Wrap(err, "error in tracker.LatestConfig")
 	}
 
 	digest, err := digester.ConfigDigest(cfg)
 	if err != nil {
-		return errors.Wrap(err, "error in digester.ConfigDigest")
+		return answer, timestamp, errors.Wrap(err, "error in digester.ConfigDigest")
 	}
 	if cfg.ConfigDigest != digest {
-		return errors.Errorf("config digest mismatch: %s (onchain) != %s (calculated)", cfg.ConfigDigest.Hex(), digest.Hex())
+		return answer, timestamp, errors.Errorf("config digest mismatch: %s (onchain) != %s (calculated)", cfg.ConfigDigest.Hex(), digest.Hex())
 	}
 
 	digest, epoch, round, answer, timestamp, err := tracker.LatestTransmissionDetails(context.TODO())
 	if err != nil {
-		return errors.Wrap(err, "error in tracker.LatestTransmissionDetails")
+		return answer, timestamp, errors.Wrap(err, "error in tracker.LatestTransmissionDetails")
 	}
 
 	bh, err := tracker.LatestBlockHeight(context.TODO())
 	if err != nil {
-		return errors.Wrap(err, "error in tracker.LatestBlockHeight")
+		return answer, timestamp, errors.Wrap(err, "error in tracker.LatestBlockHeight")
 	}
-
-	fmt.Println("LatestBlockHeight", bh)
-	fmt.Println("LatestTransmissionDetails", digest, epoch, round, answer, timestamp)
-	fmt.Println("LatestConfigBlockNumber", tracker.state.Config.LatestConfigBlockNumber)
-	fmt.Println("OffchainConfig", tracker.state.Config.OffchainConfig.Data())
-	fmt.Println("AccessControllers", tracker.state.Config.RequesterAccessController, tracker.state.Config.BillingAccessController)
-	fmt.Println("BillingConfig", tracker.state.Config.Billing.ObservationPayment, tracker.state.Config.Billing.TransmissionPayment)
-	fmt.Printf("OracleConfigs %+v\n", tracker.state.Oracles.Data())
-	fmt.Println("Transmissions Account", tracker.state.Transmissions)
-	fmt.Printf("Transmissions %+v\n", tracker.answer)
 
 	var txs TransmissionsPartial
 	err = tracker.client.rpc.GetAccountDataInto(context.TODO(), tracker.state.Transmissions, &txs)
 	seeds := [][]byte{[]byte("store"), tracker.StateID.Bytes()}
 	storeAuthority, _, err := solana.FindProgramAddress(seeds, tracker.ProgramID)
 	if err != nil {
-		return errors.Wrap(err, "error in solana.FindProgramAddress")
+		return answer, timestamp, errors.Wrap(err, "error in solana.FindProgramAddress")
 	}
-	fmt.Println("Transmissions writer permission", txs.Writer, storeAuthority)
-	fmt.Printf("Transmissions Partial: %+v\n", txs)
-	fmt.Println("Parsed Description:", string(txs.Description[:]))
 
-	return nil
+	if log {
+		fmt.Println("LatestBlockHeight", bh)
+		fmt.Println("LatestTransmissionDetails", digest, epoch, round, answer, timestamp)
+		fmt.Println("LatestConfigBlockNumber", tracker.state.Config.LatestConfigBlockNumber)
+		fmt.Println("OffchainConfig", tracker.state.Config.OffchainConfig.Data())
+		fmt.Println("AccessControllers", tracker.state.Config.RequesterAccessController, tracker.state.Config.BillingAccessController)
+		fmt.Println("BillingConfig", tracker.state.Config.Billing.ObservationPayment, tracker.state.Config.Billing.TransmissionPayment)
+		fmt.Printf("OracleConfigs %+v\n", tracker.state.Oracles.Data())
+		fmt.Println("Transmissions Account", tracker.state.Transmissions)
+		fmt.Printf("Transmissions %+v\n", tracker.answer)
+
+		// data from transmission account
+		fmt.Println("Transmissions writer permission", txs.Writer, storeAuthority)
+		fmt.Printf("Transmissions Partial: %+v\n", txs)
+		fmt.Println("Parsed Description:", string(txs.Description[:]))
+	}
+
+	return answer, timestamp, nil
 }
-
 
 // Partial transmissions state, does not include actual transmissions
 type TransmissionsPartial struct {
