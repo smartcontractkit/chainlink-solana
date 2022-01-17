75d77a16b2a6b6ce60fd2e3b8bb2fb2d34a9735d
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2022-01-17 12:37:55 +0000

    update benchmarks

--- pkg/monitoring/manager_benchmark_test.go
+++ pkg/monitoring/manager_benchmark_test.go
@@ -18,8 +18,10 @@ import (
 //goarch: amd64
 //pkg: github.com/smartcontractkit/chainlink-solana/pkg/monitoring
 //cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
-// (10jan2022)
+// (10 jan 2022)
 //    5719	    184623 ns/op	   91745 B/op	    1482 allocs/op
+// (17 jan 2022)
+//    6679	    180862 ns/op	   92230 B/op	    1493 allocs/op
 func BenchmarkManager(b *testing.B) {
 	wg := &sync.WaitGroup{}
 	defer wg.Wait()
