bd5f65576dd358cb8b63cddc3eeda7b55c9b5624
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2022-01-17 12:37:55 +0000

    update benchmarks

--- pkg/monitoring/multi_feed_monitor_benchmark_test.go
+++ pkg/monitoring/multi_feed_monitor_benchmark_test.go
@@ -22,6 +22,8 @@ import (
 //    6985	    162187 ns/op	  114802 B/op	    1506 allocs/op
 // (4 Jan 2022)
 //    9332	    166275 ns/op	  157078 B/op	    1590 allocs/op
+// (17 Jan 2022)
+//    7374	    202079 ns/op	  164301 B/op	    1712 allocs/op
 
 func BenchmarkMultichainMonitorStatePath(b *testing.B) {
 	wg := &sync.WaitGroup{}
@@ -89,6 +91,9 @@ func BenchmarkMultichainMonitorStatePath(b *testing.B) {
 // cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
 // (4 Jan 2022)
 //    61338	     18841 ns/op	    6606 B/op	     137 allocs/op
+// (17 Jan 2022)
+//    56856	     20759 ns/op	    6866 B/op	     141 allocs/op
+
 func BenchmarkMultichainMonitorTransmissionPath(b *testing.B) {
 	wg := &sync.WaitGroup{}
 	defer wg.Wait()
