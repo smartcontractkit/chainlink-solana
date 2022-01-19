fb6ca15fcf76418ddfcbee69ccbf8fed415f3a24
Evangelos Barakos <e.mparakos@gmail.com>
2022-01-19 18:08:28 +0200

    Support transfer/accept ownership commands

--- gauntlet/packages/gauntlet-solana-contracts/src/commands/contracts/ownership/acceptOwnership.ts
+++ gauntlet/packages/gauntlet-solana-contracts/src/commands/contracts/ownership/acceptOwnership.ts
@@ -1,14 +1,17 @@
 import { Result } from '@chainlink/gauntlet-core'
 import { logger, prompt } from '@chainlink/gauntlet-core/dist/utils'
-import { SolanaCommand, TransactionResponse } from '@chainlink/gauntlet-solana'
-import { PublicKey } from '@solana/web3.js'
+import { SolanaCommand, TransactionResponse, RawTransaction } from '@chainlink/gauntlet-solana'
+import { AccountMeta, PublicKey, Transaction, TransactionInstruction } from '@solana/web3.js'
 import { CONTRACT_LIST, getContract } from '../../../lib/contracts'
 import { SolanaConstructor } from '../../../lib/types'
+import { parseContractErrors, makeTx } from '../../../lib/utils'
+import { Idl } from '@project-serum/anchor'
 
 export const makeAcceptOwnershipCommand = (contractId: CONTRACT_LIST): SolanaConstructor => {
   return class AcceptOwnership extends SolanaCommand {
     static id = `${contractId}:accept_ownership`
     static category = contractId
+    idl: Idl
 
     static examples = [`yarn gauntlet ${contractId}:accept_ownership --network=devnet --state=[PROGRAM_STATE]`]
 
@@ -18,34 +21,54 @@ export const makeAcceptOwnershipCommand = (contractId: CONTRACT_LIST): SolanaCon
       this.require(!!this.flags.state, 'Please provide flags with "state"')
     }
 
-    execute = async () => {
+    makeRawTransaction = async (signer: PublicKey) => {
       const contract = getContract(contractId, '')
+      this.idl = contract.idl
       const address = contract.programId.toString()
       const program = this.loadProgram(contract.idl, address)
-
-      const owner = this.wallet.payer
 
       const state = new PublicKey(this.flags.state)
 
       await prompt(`Accepting ownership of ${contractId} state (${state.toString()}). Continue?`)
+      const data = program.coder.instruction.encode('accept_ownership', {})
 
-      const tx = await program.rpc.acceptOwnership({
-        accounts: {
-          // Store contract expects an store account instead of a state acc
-          ...(contractId === CONTRACT_LIST.STORE && { store: state }),
-          ...(contractId !== CONTRACT_LIST.STORE && { state }),
-          authority: owner.publicKey,
+      const accounts: AccountMeta[] = [
+        {
+          pubkey: state,
+          isSigner: false,
+          isWritable: true,
         },
-        signers: [owner],
-      })
+        {
+          pubkey: signer,
+          isSigner: true,
+          isWritable: false,
+        },
+      ]
 
-      logger.success(`Accepted ownership on tx ${tx}`)
+      const rawTx: RawTransaction = {
+        data,
+        accounts,
+        programId: contract.programId,
+      }
+
+      return [rawTx]
+    }
+
+    execute = async () => {
+      const rawTx = await this.makeRawTransaction(this.wallet.payer.publicKey)
+      const tx = await makeTx(rawTx)
+      logger.debug(tx)
+
+      logger.loading('Sending tx...')
+      const txhash = await parseContractErrors(this.provider.send(tx, [this.wallet.payer]), this.idl)
+  
+      logger.success(`Accepted ownership on tx ${txhash}`)
 
       return {
         responses: [
           {
-            tx: this.wrapResponse(tx, state.toString(), { state: state.toString() }),
-            contract: state.toString(),
+            tx: this.wrapResponse(txhash, this.flags.state),
+            contract: this.flags.state,
           },
         ],
       } as Result<TransactionResponse>
--- gauntlet/packages/gauntlet-solana-contracts/src/commands/contracts/ownership/transferOwnership.ts
+++ gauntlet/packages/gauntlet-solana-contracts/src/commands/contracts/ownership/transferOwnership.ts
@@ -1,14 +1,17 @@
 import { Result } from '@chainlink/gauntlet-core'
 import { logger, prompt } from '@chainlink/gauntlet-core/dist/utils'
-import { SolanaCommand, TransactionResponse } from '@chainlink/gauntlet-solana'
-import { PublicKey } from '@solana/web3.js'
+import { SolanaCommand, TransactionResponse, RawTransaction } from '@chainlink/gauntlet-solana'
+import { Idl } from '@project-serum/anchor'
+import { AccountMeta, PublicKey, Transaction, TransactionInstruction } from '@solana/web3.js'
 import { CONTRACT_LIST, getContract } from '../../../lib/contracts'
 import { SolanaConstructor } from '../../../lib/types'
+import { parseContractErrors, makeTx } from '../../../lib/utils'
 
 export const makeTransferOwnershipCommand = (contractId: CONTRACT_LIST): SolanaConstructor => {
   return class TransferOwnership extends SolanaCommand {
     static id = `${contractId}:transfer_ownership`
     static category = contractId
+    idl: Idl
 
     static examples = [
       `yarn gauntlet ${contractId}:transfer_ownership --network=devnet --state=[PROGRAM_STATE] --to=[PROPOSED_OWNER]`,
@@ -21,12 +24,11 @@ export const makeTransferOwnershipCommand = (contractId: CONTRACT_LIST): SolanaC
       this.require(!!this.flags.to, 'Please provide flags with "to"')
     }
 
-    execute = async () => {
+    makeRawTransaction = async (signer: PublicKey) => {
       const contract = getContract(contractId, '')
+      this.idl = contract.idl
       const address = contract.programId.toString()
       const program = this.loadProgram(contract.idl, address)
-
-      const owner = this.wallet.payer
 
       const state = new PublicKey(this.flags.state)
       const proposedOwner = new PublicKey(this.flags.to)
@@ -35,23 +37,47 @@ export const makeTransferOwnershipCommand = (contractId: CONTRACT_LIST): SolanaC
         `Transfering ownership of ${contractId} state (${state.toString()}) to: (${proposedOwner.toString()}). Continue?`,
       )
 
-      const tx = await program.rpc.transferOwnership(proposedOwner, {
-        accounts: {
-          // Store contract expects an store account instead of a state acc
-          ...(contractId === CONTRACT_LIST.STORE && { store: state }),
-          ...(contractId !== CONTRACT_LIST.STORE && { state }),
-          authority: owner.publicKey,
-        },
-        signers: [owner],
+      const data = program.coder.instruction.encode('transfer_ownership', {
+        proposedOwner,
       })
 
-      logger.success(`Ownership transferred to ${proposedOwner.toString()} on tx ${tx}`)
+      const accounts: AccountMeta[] = [
+        {
+          pubkey: state,
+          isSigner: false,
+          isWritable: true,
+        },
+        {
+          pubkey: signer,
+          isSigner: true,
+          isWritable: false,
+        },
+      ]
+
+      const rawTx: RawTransaction = {
+        data,
+        accounts,
+        programId: contract.programId,
+      }
+
+      return [rawTx]
+    }
+
+    execute = async () => {
+      const rawTx = await this.makeRawTransaction(this.wallet.payer.publicKey)
+      const tx = await makeTx(rawTx)
+      logger.debug(tx)
+
+      logger.loading('Sending tx...')
+      const txhash = await parseContractErrors(this.provider.send(tx, [this.wallet.payer]), this.idl)
+  
+      logger.success(`Ownership transferred to ${new PublicKey(this.flags.to)} on tx ${txhash}`)
 
       return {
         responses: [
           {
-            tx: this.wrapResponse(tx, state.toString(), { state: state.toString() }),
-            contract: state.toString(),
+            tx: this.wrapResponse(txhash, this.flags.state),
+            contract: this.flags.state,
           },
         ],
       } as Result<TransactionResponse>
