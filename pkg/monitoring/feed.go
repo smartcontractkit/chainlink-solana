326666ec56e86fd257deed9659b1915b27b5552a
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2022-01-20 16:53:33 +0000

    move the core components of the ocr2 monitoring into chainlink-relay

--- pkg/monitoring/feed.go
+++ pkg/monitoring/feed.go
@@ -1,20 +1,119 @@
 package monitoring
 
-import "io"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
 
-type FeedParser func(buf io.ReadCloser) ([]FeedConfig, error)
+	"github.com/gagliardetto/solana-go"
+	relayMonitoring "github.com/smartcontractkit/chainlink-relay/pkg/monitoring"
+)
 
-// FeedConfig is the interface for feed configurations extracted from the RDD.
-type FeedConfig interface {
-	GetName() string
-	GetPath() string
-	GetSymbol() string
-	GetHeartbeatSec() int64
-	GetContractType() string
-	GetContractStatus() string
-	// This functions as a feed identifier.
-	GetContractAddress() string
-	GetContractAddressBytes() []byte
-	// Useful for mapping to kafka messages.
-	ToMapping() map[string]interface{}
+func SolanaFeedParser(buf io.ReadCloser) ([]relayMonitoring.FeedConfig, error) {
+	rawFeeds := []SolanaFeedConfig{}
+	decoder := json.NewDecoder(buf)
+	if err := decoder.Decode(&rawFeeds); err != nil {
+		return nil, fmt.Errorf("unable to unmarshal feeds config data: %w", err)
+	}
+	feeds := make([]relayMonitoring.FeedConfig, len(rawFeeds))
+	for i, rawFeed := range rawFeeds {
+		contractAddress, err := solana.PublicKeyFromBase58(rawFeed.ContractAddressBase58)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse program id '%s' from JSON at index i=%d: %w", rawFeed.ContractAddressBase58, i, err)
+		}
+		transmissionsAccount, err := solana.PublicKeyFromBase58(rawFeed.TransmissionsAccountBase58)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse transmission account '%s' from JSON at index i=%d: %w", rawFeed.TransmissionsAccountBase58, i, err)
+		}
+		stateAccount, err := solana.PublicKeyFromBase58(rawFeed.StateAccountBase58)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse state account '%s' from JSON at index i=%d: %w", rawFeed.StateAccountBase58, i, err)
+		}
+		feeds[i] = relayMonitoring.FeedConfig(SolanaFeedConfig{
+			rawFeed.Name,
+			rawFeed.Path,
+			rawFeed.Symbol,
+			rawFeed.HeartbeatSec,
+			rawFeed.ContractType,
+			rawFeed.ContractStatus,
+
+			rawFeed.ContractAddressBase58,
+			rawFeed.TransmissionsAccountBase58,
+			rawFeed.StateAccountBase58,
+
+			contractAddress,
+			transmissionsAccount,
+			stateAccount,
+		})
+	}
+	return feeds, nil
+}
+
+type SolanaFeedConfig struct {
+	Name           string `json:"name,omitempty"`
+	Path           string `json:"path,omitempty"`
+	Symbol         string `json:"symbol,omitempty"`
+	HeartbeatSec   int64  `json:"heartbeat,omitempty"`
+	ContractType   string `json:"contract_type,omitempty"`
+	ContractStatus string `json:"status,omitempty"`
+
+	ContractAddressBase58      string `json:"contract_address_base58,omitempty"`
+	TransmissionsAccountBase58 string `json:"transmissions_account_base58,omitempty"`
+	StateAccountBase58         string `json:"state_account_base58,omitempty"`
+
+	ContractAddress      solana.PublicKey `json:"-"`
+	TransmissionsAccount solana.PublicKey `json:"-"`
+	StateAccount         solana.PublicKey `json:"-"`
+}
+
+var _ relayMonitoring.FeedConfig = SolanaFeedConfig{}
+
+func (s SolanaFeedConfig) GetName() string {
+	return s.Name
+}
+
+func (s SolanaFeedConfig) GetPath() string {
+	return s.Path
+}
+
+func (s SolanaFeedConfig) GetFeedPath() string {
+	return s.Path
+}
+
+func (s SolanaFeedConfig) GetSymbol() string {
+	return s.Symbol
+}
+
+func (s SolanaFeedConfig) GetHeartbeatSec() int64 {
+	return s.HeartbeatSec
+}
+
+func (s SolanaFeedConfig) GetContractType() string {
+	return s.ContractType
+}
+
+func (s SolanaFeedConfig) GetContractStatus() string {
+	return s.ContractStatus
+}
+
+func (s SolanaFeedConfig) GetContractAddress() string {
+	return s.ContractAddress.String()
+}
+
+func (s SolanaFeedConfig) GetContractAddressBytes() []byte {
+	return s.ContractAddress.Bytes()
+}
+
+func (s SolanaFeedConfig) ToMapping() map[string]interface{} {
+	return map[string]interface{}{
+		"feed_name":             s.Name,
+		"feed_path":             s.Path,
+		"symbol":                s.Symbol,
+		"heartbeat_sec":         int64(s.HeartbeatSec),
+		"contract_type":         s.ContractType,
+		"contract_status":       s.ContractStatus,
+		"contract_address":      s.ContractAddress.Bytes(),
+		"transmissions_account": s.TransmissionsAccount.Bytes(),
+		"state_account":         s.StateAccount.Bytes(),
+	}
 }
--- pkg/monitoring/source.go
+++ pkg/monitoring/source.go
@@ -2,32 +2,108 @@ package monitoring
 
 import (
 	"context"
-	"math/big"
-	"time"
+	"fmt"
 
-	"github.com/smartcontractkit/libocr/offchainreporting2/types"
+	relayMonitoring "github.com/smartcontractkit/chainlink-relay/pkg/monitoring"
+	pkgSolana "github.com/smartcontractkit/chainlink-solana/pkg/solana"
 )
 
-type Source interface {
-	Fetch(context.Context) (interface{}, error)
+func NewSolanaSourceFactory(log relayMonitoring.Logger) relayMonitoring.SourceFactory {
+	return &sourceFactory{log}
 }
 
-type SourceFactory interface {
-	NewSource(chainConfig ChainConfig, feedConfig FeedConfig) (Source, error)
+type sourceFactory struct {
+	log relayMonitoring.Logger
 }
 
-type Envelope struct {
-	// latest transmission details
-	ConfigDigest    types.ConfigDigest
-	Epoch           uint32
-	Round           uint8
-	LatestAnswer    *big.Int
-	LatestTimestamp time.Time
+func (s *sourceFactory) NewSource(
+	chainConfig relayMonitoring.ChainConfig,
+	feedConfig relayMonitoring.FeedConfig,
+) (relayMonitoring.Source, error) {
+	solanaConfig, ok := chainConfig.(SolanaConfig)
+	if !ok {
+		return nil, fmt.Errorf("expected chainConfig to be of type SolanaConfig not %T", chainConfig)
+	}
+	solanaFeedConfig, ok := feedConfig.(SolanaFeedConfig)
+	if !ok {
+		return nil, fmt.Errorf("expected feedConfig to be of type SolanaFeedConfig not %T", feedConfig)
+	}
+	spec := pkgSolana.OCR2Spec{
+		ProgramID: solanaFeedConfig.ContractAddress,
+		StateID:   solanaFeedConfig.StateAccount,
+	}
+	client := pkgSolana.NewClient(solanaConfig.RPCEndpoint)
+	tracker := pkgSolana.NewTracker(spec, client, nil, &logAdapter{s.log})
+	return &solanaSource{
+		&tracker,
+		solanaConfig,
+		solanaFeedConfig,
+	}, nil
+}
 
-	// latest contract config
-	ContractConfig types.ContractConfig
+type solanaSource struct {
+	tracker      *pkgSolana.ContractTracker
+	solanaConfig SolanaConfig
+	feedConfig   SolanaFeedConfig
+}
 
-	// extra
-	BlockNumber uint64
-	Transmitter types.Account
+func (s *solanaSource) Fetch(ctx context.Context) (interface{}, error) {
+	changedInBlock, _, err := s.tracker.LatestConfigDetails(ctx)
+	if err != nil {
+		return relayMonitoring.Envelope{}, fmt.Errorf("failed to fetch latest config details from on-chain: %w", err)
+	}
+	cfg, err := s.tracker.LatestConfig(ctx, changedInBlock)
+	if err != nil {
+		return relayMonitoring.Envelope{}, fmt.Errorf("failed to read latest config from on-chain: %w", err)
+	}
+	configDigest, epoch, round, latestAnswer, latestTimestamp, err := s.tracker.LatestTransmissionDetails(ctx)
+	if err != nil {
+		return relayMonitoring.Envelope{}, fmt.Errorf("failed to read latest transmission from on-chain: %w", err)
+	}
+	transmitter := s.tracker.FromAccount()
+
+	return relayMonitoring.Envelope{
+		configDigest,
+		epoch,
+		round,
+		latestAnswer,
+		latestTimestamp,
+
+		cfg,
+
+		changedInBlock,
+		transmitter,
+	}, nil
+}
+
+// Helper
+
+type logAdapter struct {
+	log relayMonitoring.Logger
+}
+
+func (l *logAdapter) Tracef(format string, values ...interface{}) {
+	l.log.Tracew(fmt.Sprintf(format, values...))
+}
+
+func (l *logAdapter) Debugf(format string, values ...interface{}) {
+	l.log.Debugw(fmt.Sprintf(format, values...))
+}
+func (l *logAdapter) Infof(format string, values ...interface{}) {
+	l.log.Infow(fmt.Sprintf(format, values...))
+}
+func (l *logAdapter) Warnf(format string, values ...interface{}) {
+	l.log.Warnw(fmt.Sprintf(format, values...))
+}
+func (l *logAdapter) Errorf(format string, values ...interface{}) {
+	l.log.Errorw(fmt.Sprintf(format, values...))
+}
+func (l *logAdapter) Criticalf(format string, values ...interface{}) {
+	l.log.Criticalw(fmt.Sprintf(format, values...))
+}
+func (l *logAdapter) Panicf(format string, values ...interface{}) {
+	l.log.Panicw(fmt.Sprintf(format, values...))
+}
+func (l *logAdapter) Fatalf(format string, values ...interface{}) {
+	l.log.Fatalw(fmt.Sprintf(format, values...))
 }
