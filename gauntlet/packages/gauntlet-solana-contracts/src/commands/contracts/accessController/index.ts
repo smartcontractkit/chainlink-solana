7f6390e700d7bd369e9e3ebf0b9395fefd97df6b
Evangelos Barakos <e.mparakos@gmail.com>
2022-01-25 18:17:24 +0200

    Merged develop

--- gauntlet/packages/gauntlet-solana-contracts/src/commands/contracts/accessController/index.ts
+++ gauntlet/packages/gauntlet-solana-contracts/src/commands/contracts/accessController/index.ts
@@ -4,6 +4,7 @@ import ReadState from './read'
 import { makeAcceptOwnershipCommand } from '../ownership/acceptOwnership'
 import { makeTransferOwnershipCommand } from '../ownership/transferOwnership'
 import { CONTRACT_LIST } from '../../../lib/contracts'
+import { makeUpgradeProgramCommand } from '../../abstract/upgrade'
 
 export default [
   Initialize,
@@ -11,4 +12,5 @@ export default [
   ReadState,
   makeAcceptOwnershipCommand(CONTRACT_LIST.ACCESS_CONTROLLER),
   makeTransferOwnershipCommand(CONTRACT_LIST.ACCESS_CONTROLLER),
+  makeUpgradeProgramCommand(CONTRACT_LIST.ACCESS_CONTROLLER),
 ]
--- gauntlet/packages/gauntlet-solana-contracts/src/lib/constants.ts
+++ gauntlet/packages/gauntlet-solana-contracts/src/lib/constants.ts
@@ -1,2 +1,5 @@
+import { PublicKey } from '@solana/web3.js'
+
 export const MAX_TRANSACTION_BYTES = 996
 export const ORACLES_MAX_LENGTH = 19
+export const UPGRADEABLE_BPF_LOADER_PROGRAM_ID = new PublicKey('BPFLoaderUpgradeab1e11111111111111111111111')
