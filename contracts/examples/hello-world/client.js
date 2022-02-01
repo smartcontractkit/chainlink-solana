d14b0bfc702c843d77297fe247adfc39dc063a04
Blaž Hrastnik <blaz@mxxn.io>
2022-02-01 13:03:53 +0900

    Use latest set of program_ids that's consistent across networks

--- contracts/examples/hello-world/client.js
+++ contracts/examples/hello-world/client.js
@@ -1,7 +1,7 @@
 const anchor = require("@project-serum/anchor");
 
 // devnet IDs
-const CHAINLINK_PROGRAM_ID = "DWqYEinRbZWtuq1DiDYvmexAKFoyjSyazZZUvdgPHT5g";
+const CHAINLINK_PROGRAM_ID = "CaH12fwNTKJAG8PxEvo9R96Zc2j8qNHZaFj8ZW49yZNT";
 // USDT/USD
 const CHAINLINK_FEED = "CwBg8pxL73LvuJ781cWBGF1e64G2z7AbZ22J2g8Lp35a";
 
--- contracts/programs/ocr2/src/lib.rs
+++ contracts/programs/ocr2/src/lib.rs
@@ -4,14 +4,7 @@ use anchor_spl::token;
 
 use arrayref::{array_ref, array_refs};
 
-#[cfg(feature = "mainnet")]
-declare_id!("My11111111111111111111111111111111111111111");
-#[cfg(feature = "testnet")]
-declare_id!("My11111111111111111111111111111111111111111");
-#[cfg(feature = "devnet")]
-declare_id!("My11111111111111111111111111111111111111111");
-#[cfg(not(any(feature = "mainnet", feature = "testnet", feature = "devnet")))]
-declare_id!("CF13pnKGJ1WJZeEgVAtFdUi4MMndXm9hneiHs8azUaZt");
+declare_id!("HW3ipKzeeduJq6f1NqRCw4doknMeWkfrM4WxobtG3o5v");
 
 mod context;
 pub mod event;
