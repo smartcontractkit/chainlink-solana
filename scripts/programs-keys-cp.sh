da86453f401bf2174461d5b0e1c5e67ad10125bb
Blaž Hrastnik <blaz@mxxn.io>
2022-02-08 15:32:19 +0900

    e2e: Generate program_ids before running tests

--- scripts/programs-keys-cp.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-network=${1:-localnet}
-
-cp contracts/artifacts/$network/*.json contracts/target/deploy
--- scripts/programs-keys-gen.sh
+++ scripts/programs-keys-gen.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
 
-set -e
+# Generate a new set of keys to use for testing. Primarily used for e2e testing on CI
+
+set -euxo pipefail
 
 network=${1:-localnet}
 
-solana-keygen new -o ./contracts/artifacts/$network/access_controller-keypair.json
-solana-keygen pubkey ./contracts/artifacts/$network/access_controller-keypair.json > ./contracts/artifacts/$network/access_controller-keypair.pub
+# solana-keygen new -o ./contracts/artifacts/$network/access_controller-keypair.json
+ACCESS_CONTROLLER_PROGRAM_ID=$(solana-keygen pubkey ./contracts/artifacts/$network/access_controller-keypair.json)
+echo $ACCESS_CONTROLLER_PROGRAM_ID > ./contracts/artifacts/$network/access_controller-keypair.pub
 
-solana-keygen new -o ./contracts/artifacts/$network/store-keypair.json
-solana-keygen pubkey ./contracts/artifacts/$network/store-keypair.json > ./contracts/artifacts/$network/store-keypair.pub
+# solana-keygen new -o ./contracts/artifacts/$network/store-keypair.json
+STORE_PROGRAM_ID=$(solana-keygen pubkey ./contracts/artifacts/$network/store-keypair.json)
+echo $STORE_PROGRAM_ID > ./contracts/artifacts/$network/store-keypair.pub
 
-solana-keygen new -o ./contracts/artifacts/$network/ocr2-keypair.json
-solana-keygen pubkey ./contracts/artifacts/$network/ocr2-keypair.json > ./contracts/artifacts/$network/ocr2-keypair.pub
+# solana-keygen new -o ./contracts/artifacts/$network/ocr2-keypair.json
+OCR2_PROGRAM_ID=$(solana-keygen pubkey ./contracts/artifacts/$network/ocr2-keypair.json)
+echo $OCR2_PROGRAM_ID > ./contracts/artifacts/$network/ocr2-keypair.pub
+
+mkdir -p ./contracts/target/deploy
+cp ./contracts/artifacts/$network/*.json ./contracts/target/deploy
+
+# Replace existing declare_id!()
+sed -i "s/DzzjdPWNfwHZmzPVxnmqkkMJraYQQRCpgFZajqkqmU6G/$ACCESS_CONTROLLER_PROGRAM_ID/" contracts/programs/access-controller/src/lib.rs
+sed -i "s/CaH12fwNTKJAG8PxEvo9R96Zc2j8qNHZaFj8ZW49yZNT/$STORE_PROGRAM_ID/" contracts/programs/store/src/lib.rs
+sed -i "s/HW3ipKzeeduJq6f1NqRCw4doknMeWkfrM4WxobtG3o5v/$OCR2_PROGRAM_ID/" contracts/programs/ocr2/src/lib.rs
