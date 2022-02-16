80ef129842d852399de98a29336a002c30bb73c9
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2022-02-16 17:06:26 +0000

    update to the latest version of the logger

--- cmd/monitoring/main.go
+++ cmd/monitoring/main.go
@@ -7,11 +7,10 @@ import (
 	relayMonitoring "github.com/smartcontractkit/chainlink-relay/pkg/monitoring"
 	"github.com/smartcontractkit/chainlink-solana/pkg/monitoring"
 	"github.com/smartcontractkit/chainlink/core/logger"
-	"go.uber.org/zap/zapcore"
 )
 
 func main() {
-	coreLog := logger.NewLogger(loggerConfig{}).With("project", "solana")
+	coreLog := logger.NewLogger().With("project", "solana")
 	log := logWrapper{coreLog}
 
 	chainConfig, err := monitoring.ParseSolanaConfig()
@@ -62,40 +61,10 @@ func main() {
 	log.Infow("monitor stopped")
 }
 
-// logger config
-
-type loggerConfig struct{}
-
-var _ logger.Config = loggerConfig{}
-
-func (l loggerConfig) RootDir() string {
-	return "" // Not logging to disk.
-}
-
-func (l loggerConfig) JSONConsole() bool {
-	return false // Logs lines are JSON formatted
-}
-
-func (l loggerConfig) LogToDisk() bool {
-	return false
-}
-
-func (l loggerConfig) LogLevel() zapcore.Level {
-	return zapcore.InfoLevel // And just like that, we now depend on zapcore!
-}
-
-func (l loggerConfig) LogUnixTimestamps() bool {
-	return false // log timestamp in ISO8601
-}
-
 // adapt core logger to monitoring logger.
 
 type logWrapper struct {
 	logger.Logger
-}
-
-func (l logWrapper) Criticalw(format string, values ...interface{}) {
-	l.Logger.CriticalW(format, values...)
 }
 
 func (l logWrapper) With(values ...interface{}) relayMonitoring.Logger {
