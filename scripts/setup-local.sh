0cfea042147be713d2cd2911aaf85ccf4bf5f0e3
aalu1418 <aalu1418@gmail.com>
2022-02-21 11:01:12 -0500

    make filepaths relative

--- scripts/setup-local.sh
+++ scripts/setup-local.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
-set -e
-source ./scripts/lib.sh
+set -euxo pipefail
+source "${BASH_SOURCE%/*}/lib.sh"
 
-ACCESS_CONTROLLER_PROGRAM_ID=$(<./contracts/artifacts/localnet/access_controller-keypair.pub)
-OCR2_PROGRAM_ID=$(<./contracts/artifacts/localnet/ocr2-keypair.pub)
-STORE_PROGRAM_ID=$(<./contracts/artifacts/localnet/store-keypair.pub)
+ACCESS_CONTROLLER_PROGRAM_ID=$(<"${BASH_SOURCE%/*}/../contracts/artifacts/localnet/access_controller-keypair.pub")
+OCR2_PROGRAM_ID=$(<"${BASH_SOURCE%/*}/../contracts/artifacts/localnet/ocr2-keypair.pub")
+STORE_PROGRAM_ID=$(<"${BASH_SOURCE%/*}/../contracts/artifacts/localnet/store-keypair.pub")
 
 modify_program $ACCESS_CONTROLLER_PROGRAM_ID $OCR2_PROGRAM_ID $STORE_PROGRAM_ID
 
 # build artifacts
-build
+build ${PWD%/}
 
 # copy build artifacts
-mkdir -p ./gauntlet/packages/gauntlet-solana-contracts/artifacts/bin
-cp ./contracts/target/deploy/*.so ./gauntlet/packages/gauntlet-solana-contracts/artifacts/bin
+mkdir -p "${BASH_SOURCE%/*}/../gauntlet/packages/gauntlet-solana-contracts/artifacts/bin"
+echo $PWD
+cp ${BASH_SOURCE%/*}/../contracts/target/deploy/*.so "${BASH_SOURCE%/*}/../gauntlet/packages/gauntlet-solana-contracts/artifacts/bin"
 
 # copy keypairs
-mkdir -p ./gauntlet/packages/gauntlet-solana-contracts/artifacts/programId
+mkdir -p "${BASH_SOURCE%/*}/../gauntlet/packages/gauntlet-solana-contracts/artifacts/programId"
 programs=("access_controller" "store" "ocr2")
 for t in ${programs[@]}; do
-  cp "./contracts/artifacts/localnet/$t-keypair.json" ./gauntlet/packages/gauntlet-solana-contracts/artifacts/programId/$t.json
+  cp "${BASH_SOURCE%/*}/../contracts/artifacts/localnet/$t-keypair.json" "${BASH_SOURCE%/*}/../gauntlet/packages/gauntlet-solana-contracts/artifacts/programId/$t.json"
 done
--- scripts/setup-staging.sh
+++ scripts/setup-staging.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-# note call from repo root
-set -e
-source ./scripts/lib.sh
-source ./gauntlet/packages/gauntlet-solana-contracts/networks/.env.staging
+set -euxo pipefail
+source "${BASH_SOURCE%/*}/lib.sh"
+source "${BASH_SOURCE%/*}/../gauntlet/packages/gauntlet-solana-contracts/networks/.env.staging"
 
 modify_program $PROGRAM_ID_ACCESS_CONTROLLER $PROGRAM_ID_OCR2 $PROGRAM_ID_STORE
 
 # build artifacts
-build
+build ${PWD%/}
