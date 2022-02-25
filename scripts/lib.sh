cff5b3ee3f5a7b77996f7c8d8b02b41e02794856
aalu1418 <aalu1418@gmail.com>
2022-02-25 17:22:35 -0500

    update to latest builds

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -6,9 +6,9 @@ function modify_program {
   store=$3
 
   # Replace existing declare_id!()
-  sed -i "s/DzzjdPWNfwHZmzPVxnmqkkMJraYQQRCpgFZajqkqmU6G/$ac/" "${BASH_SOURCE%/*}/../contracts/programs/access-controller/src/lib.rs"
-  sed -i "s/HW3ipKzeeduJq6f1NqRCw4doknMeWkfrM4WxobtG3o5v/$ocr2/" "${BASH_SOURCE%/*}/../contracts/programs/ocr2/src/lib.rs"
-  sed -i "s/CaH12fwNTKJAG8PxEvo9R96Zc2j8qNHZaFj8ZW49yZNT/$store/" "${BASH_SOURCE%/*}/../contracts/programs/store/src/lib.rs"
+  sed -i "s/9xi644bRR8birboDGdTiwBq3C7VEeR7VuamRYYXCubUW/$ac/" "${BASH_SOURCE%/*}/../contracts/programs/access-controller/src/lib.rs"
+  sed -i "s/HEvSKofvBgfaexv23kMabbYqxasxU3mQ4ibBMEmJWHny/$store/" "${BASH_SOURCE%/*}/../contracts/programs/store/src/lib.rs"
+  sed -i "s/cjg3oHmg9uuPsP8D6g29NWvhySJkdYdAo9D25PRbKXJ/$ocr2/" "${BASH_SOURCE%/*}/../contracts/programs/ocr2/src/lib.rs"
 }
 
 function build {
