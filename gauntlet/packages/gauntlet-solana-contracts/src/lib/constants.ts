0fc47ac79bf235f98ff023b283a120b426fa6318
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2022-03-02 13:56:11 +0100

    transfer instruction and create token acc command

--- gauntlet/packages/gauntlet-solana-contracts/src/lib/constants.ts
+++ gauntlet/packages/gauntlet-solana-contracts/src/lib/constants.ts
@@ -4,3 +4,5 @@ export const MAX_TRANSACTION_BYTES = 996
 export const ORACLES_MAX_LENGTH = 19
 export const UPGRADEABLE_BPF_LOADER_PROGRAM_ID = new PublicKey('BPFLoaderUpgradeab1e11111111111111111111111')
 export const ADDITIONAL_STATE_BUFFER = 1024
+
+export const TOKEN_DECIMALS = 9
