b9496633e83da7f9be41d65ecae4226d5b91f286
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2022-03-02 13:56:11 +0100

    transfer instruction and create token acc command

--- gauntlet/packages/gauntlet-solana-contracts/src/commands/contracts/token/index.ts
+++ gauntlet/packages/gauntlet-solana-contracts/src/commands/contracts/token/index.ts
@@ -1,5 +1,6 @@
 import DeployToken from './deploy'
+import CreateAccount from './createAccount'
 import ReadState from './read'
 import TransferToken from './transfer'
 
-export default [DeployToken, ReadState, TransferToken]
+export default [DeployToken, ReadState, TransferToken, CreateAccount]
