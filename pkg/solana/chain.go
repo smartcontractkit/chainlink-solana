d946bd1531280410c783b22b10fe54ab7667f160
aalu1418 <aalu1418@gmail.com>
2022-03-16 15:46:28 -0400

    add chain interface

---
+++ pkg/solana/chain.go
@@ -0,0 +1,32 @@
+package solana
+
+import (
+	"context"
+
+	"github.com/smartcontractkit/chainlink-solana/pkg/solana/client"
+)
+
+type ChainSet interface {
+	ServiceCtx
+	// Chain returns chain for the given id.
+	Chain(ctx context.Context, id string) (Chain, error)
+}
+
+type Chain interface {
+	ServiceCtx
+
+	ID() string
+	Config() Config
+	TxManager() TxManager
+	// Reader returns a new Reader. If nodeName is provided, the underlying client must use that node.
+	Reader(nodeName string) (client.Reader, error)
+}
+
+// ServiceCtx replaces Service interface due to new Start(ctx) method signature.
+type ServiceCtx interface {
+	// Start starts the service, context can be cancelled to abort Start routine.
+	Start(context.Context) error
+	Close() error
+	Ready() error
+	Healthy() error
+}
