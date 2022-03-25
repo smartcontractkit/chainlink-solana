96dadcd782126af289f8f67848a7222941a907b8
aalu1418 <aalu1418@gmail.com>
2022-03-25 16:36:28 -0400

    remove duplicate ServiceCtx definition

--- pkg/solana/chain.go
+++ pkg/solana/chain.go
@@ -5,29 +5,21 @@ import (
 
 	"github.com/smartcontractkit/chainlink-solana/pkg/solana/client"
 	"github.com/smartcontractkit/chainlink-solana/pkg/solana/config"
+	"github.com/smartcontractkit/chainlink/core/services"
 )
 
 type ChainSet interface {
-	ServiceCtx
+	services.ServiceCtx
 	// Chain returns chain for the given id.
 	Chain(ctx context.Context, id string) (Chain, error)
 }
 
 type Chain interface {
-	ServiceCtx
+	services.ServiceCtx
 
 	ID() string
 	Config() config.Config
 	TxManager() TxManager
 	// Reader returns a new Reader from the available list of nodes (if there are multiple, it will randomly select one)
 	Reader() (client.Reader, error)
-}
-
-// ServiceCtx replaces Service interface due to new Start(ctx) method signature.
-type ServiceCtx interface {
-	// Start starts the service, context can be cancelled to abort Start routine.
-	Start(context.Context) error
-	Close() error
-	Ready() error
-	Healthy() error
 }
