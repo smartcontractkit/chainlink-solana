9f73c4fc112ee6b17a65e236ec9e230fa63748c1
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2022-03-28 11:25:16 +0100

    wip

--- cmd/sub/main.go
+++ cmd/sub/main.go
@@ -2,15 +2,42 @@ package main
 
 import (
 	"context"
-	"encoding/base64"
 	"fmt"
-	"strings"
 
-	"github.com/gagliardetto/solana-go"
 	"github.com/gagliardetto/solana-go/rpc"
-	"github.com/gagliardetto/solana-go/rpc/ws"
 )
 
+func main() {
+	rpcEndpoint := "https://floral-morning-sun.solana-devnet.quiknode.pro/d874b0e33834d6babaa1e60a5b6181f22dd0409e/"
+
+	accountPubKeyBase58 := "HoLknTuGPcjsVDyEAu92x1njFKc5uUXuYLYFuhiEatF1" // testnet LINK/USD transmissions
+	//accountPubKeyBase58 := "2TQmhSnGK5NwdXBKEmJ8wfwH17rNSQgH11SVdHkYC1ZD" // testnet LINK/USD state
+	//accountPubKeyBase58 := "STGhiM1ZaLjDLZDGcVFp3ppdetggLAs6MXezw5DXXH3" // testnet LINK/USD program address
+	accountPubKey := solana.MustPublicKeyFromBase58(accountPubKeyBase58)
+
+	ctx := context.Background()
+	client := rpc.New(rpcEndpoint)
+
+	finalized, err := client.GetBlockHeight(ctx, rpc.CommitmentFinalized)
+	if err != nil {
+		panic(err)
+	}
+	//confirmed, err := client.GetBlockHeight(ctx, rpc.CommitmentConfirmed)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//processed, err := client.GetBlockHeight(ctx, rpc.CommitmentProcessed)
+	//if err != nil {
+	//	panic(err)
+	//}
+	fmt.Println(">>>>>>>> finalized", finalized)
+	//fmt.Println(">>>>>>>> confirmed", confirmed)
+	//fmt.Println(">>>>>>>> processed", processed)
+
+	client.GetSignaturesForAddressWithOpts(ctx, accountPubKey
+}
+
+/*
 func main() {
 	ctx := context.Background()
 	//wsEndpoint := "wss://4OFGJRWARF3I3HOX6ARO:VDIMBJ7OBPV5N2KWYRNNFEKEYWEQ4ODLINA7MQRR@ac135df3-af7f-45e6-9204-4c311edaf467.solana.bison.run/ws" // MALFORMED mainnet
@@ -41,20 +68,18 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		/*
-			// for account subscriptions
-			fmt.Println(">>>>>>>>>>>>>>")
-			fmt.Println("slot", res.Context.Slot)
-			fmt.Println("lamports", res.Value.Account.Lamports)
-			fmt.Println("owner", res.Value.Account.Owner)
-			fmt.Println("executable", res.Value.Account.Executable)
-			fmt.Println("rent-epoch", res.Value.Account.RentEpoch)
-			if res.Value.Account.Data != nil {
-				fmt.Println("data", res.Value.Account.Data.GetBinary()[:100])
-				fmt.Println("data size", len(res.Value.Account.Data.GetBinary()))
-			}
-			fmt.Println(">>>>>>>>>>>>>>")
-		*/
+		//	// for account subscriptions
+		//	fmt.Println(">>>>>>>>>>>>>>")
+		//	fmt.Println("slot", res.Context.Slot)
+		//	fmt.Println("lamports", res.Value.Account.Lamports)
+		//	fmt.Println("owner", res.Value.Account.Owner)
+		//	fmt.Println("executable", res.Value.Account.Executable)
+		//	fmt.Println("rent-epoch", res.Value.Account.RentEpoch)
+		//	if res.Value.Account.Data != nil {
+		//		fmt.Println("data", res.Value.Account.Data.GetBinary()[:100])
+		//		fmt.Println("data size", len(res.Value.Account.Data.GetBinary()))
+		//	}
+		//	fmt.Println(">>>>>>>>>>>>>>")
 		// for program subscriptions
 		//fmt.Println(">>>>>>>>>>>>>>", res)
 		// for logs
@@ -83,3 +108,4 @@ func main() {
 		fmt.Println(">>>>>>>>>>>>>>")
 	}
 }
+*/
