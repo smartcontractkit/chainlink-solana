9b27c3213c961d641f9b88a2869f3a3f1fece2a5
Jordan Krage <jmank88@gmail.com>
2022-03-31 18:36:48 -0500

    more tests

--- ts/src/index.test.ts
+++ ts/src/index.test.ts
@@ -1,69 +1,112 @@
-import { OCR2Feed, Round} from '.';
-import { PublicKey } from "@solana/web3.js";
+import {
+  CHAINLINK_AGGREGATOR_PROGRAM_ID,
+  CHAINLINK_STORE_PROGRAM_ID,
+  OCR2Feed,
+  Round,
+} from ".";
+import { BN } from "@project-serum/anchor";
 import * as anchor from "@project-serum/anchor";
 import * as os from "os";
 import * as fs from "fs";
 import * as path from "path";
 
-describe('OCR2Feed', () => {
-    //TODO parse tests
+describe("OCR2Feed", () => {
+  it("parseLog", () => {
+    let got = OCR2Feed.parseLog("testfeed", {
+      data: {
+        roundId: 1688241,
+        configDigest: [
+          0, 3, 244, 37, 36, 199, 185, 208, 41, 21, 110, 43, 204, 175, 70, 82,
+          5, 68, 97, 146, 252, 172, 91, 34, 130, 116, 27, 155, 249, 67, 170, 5,
+        ],
+        answer: new BN("328053000000"),
+        transmitter: 2,
+        observationsTimestamp: 1648768290,
+        observerCount: 32,
+        observers: [0, 1, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        juelsPerLamport: new BN("136747327"),
+        reimbursement: new BN("683736635000"),
+      },
+    });
+    expect(got).toEqual({
+      feed: "testfeed",
+      answer: new BN("328053000000"),
+      roundId: 1688241,
+      observationsTS: new Date("2022-03-31T23:11:30.000Z"),
+    });
+  });
+  it("parseLog null log", () => {
+    let got = OCR2Feed.parseLog("testfeed", null);
+    expect(got).toBeNull();
+  });
+  it("parseLog null data", () => {
+    let got = OCR2Feed.parseLog("testfeed", {});
+    expect(got).toBeNull();
+  });
+  it("parseLog empty data", () => {
+    let got = OCR2Feed.parseLog("testfeed", { data: {} });
+    expect(got).toBeDefined();
+    expect(got.feed).toEqual("testfeed");
+    expect(got.answer).toBeUndefined();
+    expect(got.roundId).toBeUndefined();
+    expect(got.observationsTS).toBeUndefined();
+  });
 
-    let itWS = process.env.ANCHOR_PROVIDER_URL ? it : it.skip
-    let programID = new PublicKey("STGhiM1ZaLjDLZDGcVFp3ppdetggLAs6MXezw5DXXH3")
-    let feed = new PublicKey("HoLknTuGPcjsVDyEAu92x1njFKc5uUXuYLYFuhiEatF1");
-    itWS(
-        'onRound',
-        async () => {
-            // Temp dir
-            let tmpResolve
-            let tmpPromise = new Promise<string>((r) => (tmpResolve = r))
-            fs.mkdtemp(path.join(os.tmpdir(), 'anchor-wallet-'), (err, folder) => {
-                if (err) {
-                    console.log(err)
-                    tmpResolve("")
-                    return
-                }
-                tmpResolve(folder)
-            })
-            let tmpDir = await tmpPromise
-            expect(tmpDir).toBeDefined()
+  let itWS = process.env.ANCHOR_PROVIDER_URL ? it : it.skip;
+  itWS(
+    "onRound",
+    async () => {
+      // Temp dir
+      let tmpResolve;
+      let tmpPromise = new Promise<string>((r) => (tmpResolve = r));
+      fs.mkdtemp(path.join(os.tmpdir(), "anchor-wallet-"), (err, folder) => {
+        if (err) {
+          console.log(err);
+          tmpResolve("");
+          return;
+        }
+        tmpResolve(folder);
+      });
+      let tmpDir = await tmpPromise;
+      expect(tmpDir).toBeDefined();
 
-            // Temp key
-            let keyPair = anchor.web3.Keypair.generate()
-            process.env.ANCHOR_WALLET = path.join(tmpDir,"test-anchor-key")
-            let doneResolve
-            let donePromise = new Promise<boolean>((r)=>(doneResolve = r))
+      // Temp key
+      let keyPair = anchor.web3.Keypair.generate();
+      process.env.ANCHOR_WALLET = path.join(tmpDir, "test-anchor-key");
+      let doneResolve;
+      let donePromise = new Promise<boolean>((r) => (doneResolve = r));
 
-            let json = JSON.stringify(Array.from(keyPair.secretKey))
-            fs.writeFile(process.env.ANCHOR_WALLET, json, (err) => {
-                if (err) {
-                    console.log(err)
-                    doneResolve(false)
-                    return
-                }
-                doneResolve(true)
-            })
-            let done = await donePromise
-            expect(done).toBeTruthy()
+      let json = JSON.stringify(Array.from(keyPair.secretKey));
+      fs.writeFile(process.env.ANCHOR_WALLET, json, (err) => {
+        if (err) {
+          console.log(err);
+          doneResolve(false);
+          return;
+        }
+        doneResolve(true);
+      });
+      let done = await donePromise;
+      expect(done).toBeTruthy();
 
-            // Listen to feed
-            let cl = await OCR2Feed.load(programID, anchor.Provider.env())
-            let resolve
-            let promise = new Promise<Round>((r) => (resolve = r))
-            let num
-            num = cl.onRound(feed, (round) => {
-                resolve(round)
-                cl.removeListener(num);
-            })
-            let got = await promise
-            expect(got).toBeDefined()
-            expect(got.contract).toEqual(feed)
-            expect(got.answer).toBeDefined()
-            expect(got.roundId).toBeDefined()
-            expect(got.epoch).toBeDefined()
-            expect(got.aggregatorRoundId).toBeDefined()
-            expect(got.observationsTS).toBeDefined()
-        },
-        20_000,
-    )
-})
+      // Listen to feed
+      let cl = await OCR2Feed.load(
+        CHAINLINK_AGGREGATOR_PROGRAM_ID,
+        anchor.Provider.env()
+      );
+      let resolve;
+      let promise = new Promise<Round>((r) => (resolve = r));
+      let num;
+      num = cl.onRound(CHAINLINK_STORE_PROGRAM_ID, (round) => {
+        resolve(round);
+        cl.removeListener(num);
+      });
+      let got = await promise;
+      expect(got).toBeDefined();
+      expect(got.feed).toEqual(CHAINLINK_STORE_PROGRAM_ID);
+      expect(got.answer).toBeDefined();
+      expect(got.roundId).toBeDefined();
+      expect(got.observationsTS).toBeDefined();
+    },
+    20_000
+  );
+});
--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,17 +1,19 @@
 import * as anchor from "@project-serum/anchor";
 import { BN } from "@project-serum/anchor";
-import {  PublicKey } from "@solana/web3.js";
+import { PublicKey } from "@solana/web3.js";
 
-export const CHAINLINK_AGGREGATOR_PROGRAM_ID = new PublicKey("HW3ipKzeeduJq6f1NqRCw4doknMeWkfrM4WxobtG3o5v");
-export const CHAINLINK_STORE_PROGRAM_ID = new PublicKey("CaH12fwNTKJAG8PxEvo9R96Zc2j8qNHZaFj8ZW49yZNT");
+export const CHAINLINK_AGGREGATOR_PROGRAM_ID = new PublicKey(
+  "HW3ipKzeeduJq6f1NqRCw4doknMeWkfrM4WxobtG3o5v"
+);
+export const CHAINLINK_STORE_PROGRAM_ID = new PublicKey(
+  "CaH12fwNTKJAG8PxEvo9R96Zc2j8qNHZaFj8ZW49yZNT"
+);
 
 export interface Round {
-  contract: PublicKey
-  answer: BN
-  roundId: number
-  epoch: number
-  aggregatorRoundId: number
-  observationsTS: Date
+  feed: PublicKey;
+  answer: BN;
+  roundId: number;
+  observationsTS: Date;
 }
 
 export class OCR2Feed {
@@ -19,14 +21,17 @@ export class OCR2Feed {
 
   constructor(
     readonly aggregatorProgram: anchor.Program,
-    readonly provider: anchor.Provider,
+    readonly provider: anchor.Provider
   ) {
-    this._parser = new anchor.EventParser(aggregatorProgram.programId, aggregatorProgram.coder);
+    this._parser = new anchor.EventParser(
+      aggregatorProgram.programId,
+      aggregatorProgram.coder
+    );
   }
 
   static async load(
     programID: PublicKey = CHAINLINK_AGGREGATOR_PROGRAM_ID,
-    provider: anchor.Provider = anchor.getProvider(),
+    provider: anchor.Provider = anchor.getProvider()
   ): Promise<OCR2Feed> {
     const aggregatorProgram = await anchor.Program.at(programID, provider);
     return new OCR2Feed(aggregatorProgram, provider);
@@ -34,27 +39,36 @@ export class OCR2Feed {
 
   public onRound(feed: PublicKey, callback: (round: Round) => void): number {
     return this.provider.connection.onLogs(feed, (event, ctx) => {
-        this._parser.parseLogs(event.logs, (log) => {
-          if (log.name != "NewTransmission") {
-            return;
-          }
-          let answer = log.data.answer as Uint8Array;
-          let event = {
-            ...log.data,
-            answer: new BN(answer, 10, 'le'),
-            slot: ctx.slot, //TODO ?
-            contract: feed,
-            roundId: -1, //TODO
-            epoch: -1, //TODO
-            aggregatorRoundId: -1, //TODO
-            observationsTS: new Date(),
-          };
-          callback(event);
-        })
-    })
+      this._parser.parseLogs(event.logs, (log) => {
+        if (log.name != "NewTransmission") {
+          return;
+        }
+        callback(OCR2Feed.parseLog(feed, log));
+      });
+    });
   }
 
   public async removeListener(listener: number): Promise<void> {
-    return this.provider.connection.removeOnLogsListener(listener)
+    return this.provider.connection.removeOnLogsListener(listener);
+  }
+
+  public static parseLog(feed, log): Round {
+    if (!log || !log.data) return null;
+    let answer: BN;
+    if (log.data.answer)
+      answer = new BN(log.data.answer as Uint8Array, 10, "le");
+    let roundId: number;
+    if (log.data.roundId) roundId = log.data.roundId as number;
+    let observationsTS: Date;
+    if (log.data.observationsTimestamp)
+      observationsTS = new Date(
+        (log.data.observationsTimestamp as number) * 1000
+      );
+    return {
+      feed: feed,
+      answer: answer,
+      roundId: roundId,
+      observationsTS: observationsTS,
+    };
   }
 }
