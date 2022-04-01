216fc2b7c021c9a1dcf9afbc1122b52383f0ab8d
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2022-04-01 12:13:17 +0100

    wip work on deserializing logs

--- cmd/ingestor/main.go
+++ cmd/ingestor/main.go
@@ -3,6 +3,84 @@ package main
 import (
 	"context"
 	"fmt"
+
+	"github.com/gagliardetto/solana-go"
+	"github.com/gagliardetto/solana-go/rpc"
+	"github.com/gagliardetto/solana-go/rpc/ws"
+)
+
+func main() {
+	ctx := context.Background()
+	//wsEndpoint := "wss://4OFGJRWARF3I3HOX6ARO:VDIMBJ7OBPV5N2KWYRNNFEKEYWEQ4ODLINA7MQRR@ac135df3-af7f-45e6-9204-4c311edaf467.solana.bison.run/ws" // MALFORMED mainnet
+	//wsEndpoint := "wss://wispy-bold-water.solana-mainnet.quiknode.pro/01b51251bd130abae974c0cc72d79f068c133416/" // mainnet
+	wsEndpoint := "wss://floral-morning-sun.solana-devnet.quiknode.pro/d874b0e33834d6babaa1e60a5b6181f22dd0409e/" // testnet
+	client, err := ws.Connect(ctx, wsEndpoint)
+	if err != nil {
+		panic(err)
+	}
+	//accountPubKeyBase58 := "HoLknTuGPcjsVDyEAu92x1njFKc5uUXuYLYFuhiEatF1" // testnet LINK/USD transmissions
+	//accountPubKeyBase58 := "2TQmhSnGK5NwdXBKEmJ8wfwH17rNSQgH11SVdHkYC1ZD" // testnet LINK/USD state
+	accountPubKeyBase58 := "STGhiM1ZaLjDLZDGcVFp3ppdetggLAs6MXezw5DXXH3" // testnet LINK/USD program address
+	accountPubKey := solana.MustPublicKeyFromBase58(accountPubKeyBase58)
+	//commitment := rpc.CommitmentConfirmed // No values received
+	commitment := rpc.CommitmentProcessed
+	//sub, err := client.AccountSubscribe(accountPubKey, commitment)
+	//sub, err := client.ProgramSubscribe(accountPubKey, commitment)
+	sub, err := client.LogsSubscribeMentions(accountPubKey, commitment)
+	if err != nil {
+		panic(err)
+	}
+	for {
+		res, err := sub.Recv()
+		if err != nil {
+			panic(err)
+		}
+		//	// for account subscriptions
+		//	fmt.Println(">>>>>>>>>>>>>>")
+		//	fmt.Println("slot", res.Context.Slot)
+		//	fmt.Println("lamports", res.Value.Account.Lamports)
+		//	fmt.Println("owner", res.Value.Account.Owner)
+		//	fmt.Println("executable", res.Value.Account.Executable)
+		//	fmt.Println("rent-epoch", res.Value.Account.RentEpoch)
+		//	if res.Value.Account.Data != nil {
+		//		fmt.Println("data", res.Value.Account.Data.GetBinary()[:100])
+		//		fmt.Println("data size", len(res.Value.Account.Data.GetBinary()))
+		//	}
+		//	fmt.Println(">>>>>>>>>>>>>>")
+		// for program subscriptions
+		//fmt.Println(">>>>>>>>>>>>>>", res)
+		// for logs
+		fmt.Println(">>>>>>>>>>>>>>")
+		fmt.Println("Slot", res.Context.Slot)
+		fmt.Println("Signature", res.Value.Signature[:])
+		fmt.Println("Err", res.Value.Err)
+		for i, log := range res.Value.Logs {
+			fmt.Printf("Log %d: %s\n", i, log)
+			//if strings.HasPrefix(log, "Program log:") && !strings.HasPrefix(log, "Program log: Instruction:") {
+			//	payload := strings.TrimPrefix(log, "Program log: ")
+			//	fmt.Printf("Payload %d: %#v\n", i, payload)
+			//	//buf, err := base64.RawStdEncoding.DecodeString(payload)
+			//	//buf, err := base64.RawURLEncoding.DecodeString(payload)
+			//	buf, err := base64.StdEncoding.DecodeString(payload)
+			//	//buf, err := base64.URLEncoding.DecodeString(payload)
+			//	if err != nil {
+			//		fmt.Println("Error:", err)
+			//	} else {
+			//		fmt.Println("Decoded:", string(buf))
+			//	}
+			//}
+		}
+		//fmt.Printf("Logs %#v\n", res.Value.Logs)
+		fmt.Println(">>>>>>>>>>>>>>")
+	}
+}
+
+/*
+package main
+
+import (
+	"context"
+	"fmt"
 	"log"
 
 	"github.com/gagliardetto/solana-go"
@@ -60,3 +138,4 @@ type logWrapper struct {
 func (l logWrapper) With(values ...interface{}) relayMonitoring.Logger {
 	return logWrapper{l.Logger.With(values...)}
 }
+*/
--- pkg/monitoring/updater_logs.go
+++ pkg/monitoring/updater_logs.go
@@ -2,7 +2,12 @@ package monitoring
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"regexp"
 
+	bin "github.com/gagliardetto/binary"
 	"github.com/gagliardetto/solana-go"
 	"github.com/gagliardetto/solana-go/rpc/ws"
 	relayMonitoring "github.com/smartcontractkit/chainlink-relay/pkg/monitoring"
@@ -49,7 +54,7 @@ SUBSCRIBE_LOOP:
 				Slot:      result.Context.Slot,
 				Signature: result.Value.Signature[:],
 				Err:       result.Value.Err,
-				Logs:      filterAndDeserializeLogs(result.Value.Logs),
+				Logs:      filterAndDeserializeLogs(result.Value.Logs, program),
 			}
 			select {
 			case l.updates <- log:
@@ -67,6 +72,90 @@ func (l *logsUpdater) Updates() <-chan interface{} {
 
 // Helpers
 
-func filterAndDeserializeLogs(logs []string) []string {
+var programInvocation = regexp.MustCompile("^Program\\s([a-zA-Z0-9])+?\\sinvoke\\s[\\d]$")
+var programFinish = regexp.MustCompile("^Program\\s([a-zA-Z0-9])+?\\s(success|error)$")
+var programLogEvent = regexp.MustCompile("^Program\\s(log|data):\\s([+/0-9A-Za-z]+={0,2})?$")
 
+func filterLogs(logs []string, programID string) []string {
+	invocationStack := []string{}
+	output := []string{}
+	for _, log := range logs {
+		if matches := programInvocation.FindStringSubmatch(log); matches != nil {
+			invokedProgramID := matches[1]
+			invocationStack = append(invocationStack, invokedProgramID)
+		} else if matches := programFinished.FindStringSubmatch(log); matches != nil {
+			finishedProgramID := matches[1]
+			if invocationStack[len(invocationStack)-1] != finishedProgramID {
+				// Oh noes!
+			} else {
+				invocationStack = invocationStack[:len(invocationStack)-1]
+			}
+		} else if matches := programLogEvent.FindStringSubmatch(log); matches != nil {
+			currentProgramID := invocationStack[len(invocationStack)-1]
+			if programID == currentProgramID {
+				output = append(output, matches[1])
+			}
+		}
+	}
+	return output
+}
+
+func deserilizeLogs(logs []string) ([]interface{}, error) {
+	for _, log := range logs {
+		buf, err := base64.StdEncoding.DecodeString(log)
+		if err != nil {
+			return nil, err
+		}
+		switch buf[:8] {
+		case SetConfigDiscriminator:
+		case SetBillingDiscriminator:
+		case RoundRequestedDiscriminator:
+		case NewTransmissionDiscriminator:
+		}
+		bin.NewDecoder(buf).Decode
+	}
+}
+
+var (
+	SetConfigDiscriminator       []byte
+	SetBillingDiscriminator      []byte
+	RoundRequestedDiscriminator  []byte
+	NewTransmissionDiscriminator []byte
+)
+
+func init() {
+	SetConfigDiscriminator = sha256.Sum256(fmt.Sprintf("event:SetConfig"))[:8]
+	SetBillingDiscriminator = sha256.Sum256(fmt.Sprintf("event:SetBilling"))[:8]
+	RoundRequestedDiscriminator = sha256.Sum256(fmt.Sprintf("event:RoundRequested"))[:8]
+	NewTransmissionDiscriminator = sha256.Sum256(fmt.Sprintf("event:NewTransmission"))[:8]
+}
+
+type SetConfig struct {
+	ConfigDigest [32]uint8   `json:"config_digest,omitempty"`
+	F            uint8       `json:"f,omitempty"`
+	Signers      [][20]uint8 `json:"signers,omitempty"`
+}
+
+type SetBilling struct {
+	ObservationPaymentGJuels  uint32 `json:"observation_payment_gjuels,omitempty"`
+	TransmissionPaymentGJuels uint32 `json:"transmission_payment_gjuels,omitempty"`
+}
+
+type RoundRequested struct {
+	ConfigDigest [32]uint8        `json:"config_digest,omitempty"`
+	Requester    solana.PublicKey `json:"requester,omitempty"`
+	Epoch        uint32           `json:"epoch,omitempty"`
+	Round        uint8            `json:"round,omitempty"`
+}
+
+type NewTransmission struct {
+	RoundID               uint32     `json:"round_id,omitempty"`
+	ConfigDigest          [32]uint8  `json:"config_digest,omitempty"`
+	Answer                bin.Int128 `json:"answer,omitempty"`
+	Transmitter           uint8      `json:"transmitter,omitempty"`
+	ObservationsTimestamp uint32     `json:"observations_timestamp,omitempty"`
+	ObserverCount         uint8      `json:"observer_count,omitempty"`
+	Observers             [19]uint8  `json:"observers,omitempty"`
+	JuelsPerLamport       uint64     `json:"juels_per_lamport,omitempty"`
+	ReimbursementGJuels   uint64     `json:"reimbursement_gjuels,omitempty"`
 }
