7426933584cec81cd99c7b927a6b6105461b2536
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2022-04-04 13:52:09 +0100

    update cmd/monitoring to introduce a new run mode for the ingestor process

--- cmd/monitoring/README.md
+++ cmd/monitoring/README.md
@@ -55,7 +55,6 @@ KAFKA_SECURITY_PROTOCOL="PLAINTEXT" \
 KAFKA_SASL_MECHANISM="PLAIN" \
 KAFKA_SASL_USERNAME="" \
 KAFKA_SASL_PASSWORD="" \
-KAFKA_CONFIG_SET_TOPIC="config_set" \
 KAFKA_CONFIG_SET_SIMPLIFIED_TOPIC="config_set_simplified" \
 KAFKA_TRANSMISSION_TOPIC="transmission_topic" \
 SCHEMA_REGISTRY_URL="http://localhost:8989" \
@@ -63,9 +62,7 @@ SCHEMA_REGISTRY_USERNAME="" \
 SCHEMA_REGISTRY_PASSWORD="" \
 HTTP_ADDRESS="localhost:3000" \
 FEEDS_URL="http://localhost:4000" \
-FEATURE_TEST_ONLY_FAKE_READERS=true \
-FEATURE_TEST_ONLY_FAKE_RDD=true \
-go run ./cmd/monitoring/main.go
+go run ./cmd/monitoring/main.go --run-mode=monitor
 ```
 
 See `go run ./cmd/monitoring/*.go -help` for details.
@@ -76,4 +73,67 @@ To generate random data instead of reading from the chain, use the env var `TEST
 
 ```bash
 docker build -f ./ops/monitoring/Dockerfile -t solana-onchain-monitor:0.1.0 .
+```
+
+# Solana Ingestor
+
+## Running
+
+```bash
+SOLANA_WS_ENDPOINT="wss:127.0.0.1:8899" \
+SOLANA_NETWORK_NAME="solana-devnet" \
+SOLANA_NETWORK_ID="solana-devnet" \
+SOLANA_CHAIN_ID="1" \
+SOLANA_STATE_KAFKA_TOPIC="state" \
+SOLANA_TRANSMISSIONS_KAFKA_TOPIC="transmissions" \
+SOLANA_EVENTS_KAFKA_TOPIC="events" \
+KAFKA_BROKERS="localhost:29092" \
+KAFKA_CLIENT_ID="solana" \
+KAFKA_SECURITY_PROTOCOL="PLAINTEXT" \
+KAFKA_SASL_MECHANISM="PLAIN" \
+KAFKA_SASL_USERNAME="" \
+KAFKA_SASL_PASSWORD="" \
+KAFKA_CONFIG_SET_SIMPLIFIED_TOPIC="config_set_simplified" \
+KAFKA_TRANSMISSION_TOPIC="transmission_topic" \
+SCHEMA_REGISTRY_URL="http://localhost:8989" \
+SCHEMA_REGISTRY_USERNAME="" \
+SCHEMA_REGISTRY_PASSWORD="" \
+HTTP_ADDRESS="localhost:3000" \
+FEEDS_URL="http://localhost:4000" \
+go run ./cmd/monitoring/main.go --run-mode=ingesto
+```
+
+## List of events
+- These events are only only issued by the ocr2 contract.
+
+```
+SetConfig {
+    pub config_digest: [u8; 32],
+    pub f: u8,
+    pub signers: Vec<[u8; 20]>,
+}
+
+SetBilling {
+    pub observation_payment_gjuels: u32,
+    pub transmission_payment_gjuels: u32,
+}
+
+RoundRequested {
+    pub config_digest: [u8; 32],
+    pub requester: Pubkey,
+    pub epoch: u32,
+    pub round: u8,
+}
+
+NewTransmission {
+    pub round_id: u32,
+    pub config_digest: [u8; 32],
+    pub answer: i128,
+    pub transmitter: u8,
+    pub observations_timestamp: u32,
+    pub observer_count: u8,
+    pub observers: [u8; 19], // Can't use MAX_ORACLES because of IDL parsing issues
+    pub juels_per_lamport: u64,
+    pub reimbursement_gjuels: u64,
+}
 ```
--- cmd/monitoring/main.go
+++ cmd/monitoring/main.go
@@ -4,9 +4,15 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	"github.com/gagliardetto/solana-go/rpc"
+	"github.com/gagliardetto/solana-go/rpc/ws"
 	relayMonitoring "github.com/smartcontractkit/chainlink-relay/pkg/monitoring"
+	"github.com/smartcontractkit/chainlink-relay/pkg/monitoring/config"
 	"github.com/smartcontractkit/chainlink/core/logger"
 
 	"github.com/smartcontractkit/chainlink-solana/pkg/monitoring"
@@ -19,12 +25,21 @@ func main() {
 			log.Println(fmt.Sprintf("Error while closing Logger: %v", err))
 		}
 	}()
-	l := logWrapper{coreLog}
 
 	chainConfig, err := monitoring.ParseSolanaConfig()
 	if err != nil {
-		l.Fatalw("failed to parse solana-specific config", "error", err)
+		coreLog.Fatalw("failed to parse solana-specific config", "error", err)
 	}
+
+	if chainConfig.RunMode == "monitor" {
+		runMonitor(chainConfig, coreLog)
+	} else if chainConfig.RunMode == "ingestor" {
+		runIngestor(chainConfig, coreLog)
+	}
+}
+
+func runMonitor(chainConfig monitoring.SolanaConfig, coreLog logger.Logger) {
+	l := logWrapper{coreLog}
 
 	client := rpc.New(chainConfig.RPCEndpoint)
 
@@ -64,6 +79,121 @@ func main() {
 
 	monitor.Run()
 	l.Infow("monitor stopped")
+}
+
+func runIngestor(chainConfig monitoring.SolanaConfig, coreLog logger.Logger) {
+	wg := &sync.WaitGroup{}
+	rootCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	log := logWrapper{coreLog}
+
+	client, err := ws.Connect(rootCtx, chainConfig.WSEndpoint)
+	if err != nil {
+		log.Fatalw("failed to connect to WS server", "error", err)
+	}
+	defer client.Close()
+
+	cfg, err := config.Parse()
+	if err != nil {
+		log.Fatalw("failed to parse generic configuration", "error", err)
+	}
+
+	producer, err := relayMonitoring.NewProducer(rootCtx, log.With("component", "producer"), cfg.Kafka)
+	if err != nil {
+		log.Fatalw("failed to create kafka producer", "error", err)
+	}
+	// TODO instrumented producer
+
+	schemaRegistry := relayMonitoring.NewSchemaRegistry(cfg.SchemaRegistry, log)
+
+	stateSchema, err := schemaRegistry.EnsureSchema(
+		relayMonitoring.SubjectFromTopic(chainConfig.StateKafkaTopic), monitoring.StateAvroSchema)
+	if err != nil {
+		log.Fatalf("failed to prepare state schema", "error", err)
+	}
+	transmissionsSchema, err := schemaRegistry.EnsureSchema(
+		relayMonitoring.SubjectFromTopic(chainConfig.TransmissionsKafkaTopic), monitoring.TransmissionsAvroSchema)
+	if err != nil {
+		log.Fatalf("failed to prepare transmissions schema", "error", err)
+	}
+	eventsSchema, err := schemaRegistry.EnsureSchema(
+		relayMonitoring.SubjectFromTopic(chainConfig.EventsKafkaTopic), monitoring.EventsAvroSchema)
+	if err != nil {
+		log.Fatalf("failed to prepare transmissions schema", "error", err)
+	}
+
+	rddSource := relayMonitoring.NewRDDSource(
+		cfg.Feeds.URL,
+		monitoring.SolanaFeedParser,
+		log.With("component", "rdd-source"),
+		cfg.Feeds.IgnoreIDs,
+	)
+
+	rddPoller := relayMonitoring.NewSourcePoller(
+		rddSource,
+		log.With("component", "rdd-poller"),
+		cfg.Feeds.RDDPollInterval,
+		cfg.Feeds.RDDReadTimeout,
+		0, // no buffering!
+	)
+
+	manager := relayMonitoring.NewManager(
+		log.With("component", "manager"),
+		rddPoller,
+	)
+
+	// Configure HTTP server
+	httpServer := relayMonitoring.NewHTTPServer(rootCtx, cfg.HTTP.Address, log.With("component", "http-server"))
+	httpServer.Handle("/debug", manager.HTTPHandler())
+	// TODO (dru) add metrics
+
+	processor := monitoring.NewIngestor(
+		chainConfig,
+		client,
+		rpc.CommitmentConfirmed,
+		producer,
+		stateSchema,
+		transmissionsSchema,
+		eventsSchema,
+		log,
+	)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		rddPoller.Run(rootCtx)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		httpServer.Run(rootCtx)
+	}()
+
+	// Handle signals from the OS
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		osSignalsCh := make(chan os.Signal, 1)
+		signal.Notify(osSignalsCh, syscall.SIGINT, syscall.SIGTERM)
+		select {
+		case sig := <-osSignalsCh:
+			log.Infow("received signal. Stopping", "signal", sig)
+			cancel()
+		case <-rootCtx.Done():
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		manager.Run(rootCtx, func(localCtx context.Context, feeds []relayMonitoring.FeedConfig) {
+			processor.Run(localCtx, feeds)
+		})
+	}()
+
+	wg.Wait()
 }
 
 // adapt core logger to monitoring logger.
