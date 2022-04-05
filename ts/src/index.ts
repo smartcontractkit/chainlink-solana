8196924876c0cfd5838fc508cac9a00d319993f4
Blaž Hrastnik <blaz@mxxn.io>
2022-04-05 23:23:22 +0900

    solana-sdk: Typescript client that can register a callback to feed rounds (#224)
    * Typescript client that can register a callback to feed rounds
    
    * test case calls module script
    
    * move to proper position to run test case
    
    * ts: OCR2Feed and Round; wip tests
    
    * Fixes test & build config
    
    * streaming and passing simple test
    
    * more tests
    
    * ci: Compile the typescript client before use
    
    * ts: ts client specific config should not mess with gauntlet config
    
    * gaunlet -> gauntlet
    
    * ??
    
    * Fix import
    
    * Updated yarn.lock
    
    * Passing test for the newest contract + onEvents via client
    
    * Update ids
    
    Co-authored-by: aalu1418 <aalu1418@gmail.com>
    Co-authored-by: Jordan Krage <jmank88@gmail.com>
    Co-authored-by: Ainsley Chang <24637818+ainsleybc@users.noreply.github.com>

---
+++ ts/src/index.ts
@@ -0,0 +1,74 @@
+import * as anchor from "@project-serum/anchor";
+import { BN } from "@project-serum/anchor";
+import { PublicKey } from "@solana/web3.js";
+
+export const CHAINLINK_AGGREGATOR_PROGRAM_ID = new PublicKey(
+  "cjg3oHmg9uuPsP8D6g29NWvhySJkdYdAo9D25PRbKXJ"
+);
+export const CHAINLINK_STORE_PROGRAM_ID = new PublicKey(
+  "HEvSKofvBgfaexv23kMabbYqxasxU3mQ4ibBMEmJWHny"
+);
+
+export interface Round {
+  feed: PublicKey;
+  answer: BN;
+  roundId: number;
+  observationsTS: Date;
+}
+
+export class OCR2Feed {
+  private _parser: anchor.EventParser;
+
+  constructor(
+    readonly aggregatorProgram: anchor.Program,
+    readonly provider: anchor.Provider
+  ) {
+    this._parser = new anchor.EventParser(
+      aggregatorProgram.programId,
+      aggregatorProgram.coder
+    );
+  }
+
+  static async load(
+    programID: PublicKey = CHAINLINK_AGGREGATOR_PROGRAM_ID,
+    provider: anchor.Provider = anchor.getProvider()
+  ): Promise<OCR2Feed> {
+    const aggregatorProgram = await anchor.Program.at(programID, provider);
+    return new OCR2Feed(aggregatorProgram, provider);
+  }
+
+  public onRound(feed: PublicKey, callback: (round: Round) => void): number {
+    return this.provider.connection.onLogs(feed, (event, ctx) => {
+      this._parser.parseLogs(event.logs, (log) => {
+        if (log.name != "NewTransmission") {
+          return;
+        }
+        callback(OCR2Feed.parseLog(feed, log));
+      });
+    });
+  }
+
+  public async removeListener(listener: number): Promise<void> {
+    return this.provider.connection.removeOnLogsListener(listener);
+  }
+
+  public static parseLog(feed, log): Round {
+    if (!log || !log.data) return null;
+    let answer: BN;
+    if (log.data.answer)
+      answer = new BN(log.data.answer as Uint8Array, 10, "le");
+    let roundId: number;
+    if (log.data.roundId) roundId = log.data.roundId as number;
+    let observationsTS: Date;
+    if (log.data.observationsTimestamp)
+      observationsTS = new Date(
+        (log.data.observationsTimestamp as number) * 1000
+      );
+    return {
+      feed: feed,
+      answer: answer,
+      roundId: roundId,
+      observationsTS: observationsTS,
+    };
+  }
+}
