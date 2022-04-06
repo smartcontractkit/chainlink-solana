fe26bb539d87e69e471658bddca5a80fbb28a3b0
Kyle Martin <kylesmartin19@gmail.com>
2022-04-06 10:28:16 -0400

    Add logs about latest transmission

--- gauntlet/packages/gauntlet-solana-contracts/src/commands/contracts/ocr2/inspection/inspectResponses.ts
+++ gauntlet/packages/gauntlet-solana-contracts/src/commands/contracts/ocr2/inspection/inspectResponses.ts
@@ -1,8 +1,9 @@
 import { Result } from '@chainlink/gauntlet-core'
 import { SolanaCommand, TransactionResponse } from '@chainlink/gauntlet-solana'
 import { PublicKey } from '@solana/web3.js'
+import { deserializeConfig } from '../../../../lib/encoding'
 import { CONTRACT_LIST, getContract } from '../../../../lib/contracts'
-import { inspection } from '@chainlink/gauntlet-core/dist/utils'
+import { inspection, logger, BN } from '@chainlink/gauntlet-core/dist/utils'
 
 type Input = {}
 
@@ -31,7 +32,22 @@ export default class OCR2InspectResponses extends SolanaCommand {
 
     const state = new PublicKey(this.args[0])
     const input = this.makeInput(this.flags.input)
-    const data = await program.account.state.fetch(state)
+    const onChainState = await program.account.state.fetch(state)
+
+    logger.info(`Latest Transmission:
+  - Latest Transmitter: ${onChainState.config.latestTransmitter}
+  - Latest Aggregator Round ID: ${onChainState.config.latestAggregatorRoundId}
+  - Latest Config Digest: ${onChainState.config.latestConfigDigest}
+  - Latest Config Block Number: ${onChainState.config.latestConfigBlockNumber}`)
+
+  /*
+    const bufferedConfig = Buffer.from(onChainState.offchainConfig.xs).slice(
+      0,
+      new BN(onChainState.offchainConfig.len).toNumber(),
+    )
+    const onChainOCRConfig = deserializeConfig(bufferedConfig)
+    console.log(onChainOCRConfig)
+    */
 
     const inspections: inspection.Inspection[] = []
 
