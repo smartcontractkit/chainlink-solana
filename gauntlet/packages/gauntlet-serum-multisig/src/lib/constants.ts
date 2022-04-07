f54f0eb7e6aeb6431433964e2233267480ddb74b
kylesmartin <54827727+kylesmartin@users.noreply.github.com>
2022-04-07 09:46:42 -0400

    Remove constants that are not used
    Co-authored-by: Rodrigo Ariza <15104916+RodrigoAD@users.noreply.github.com>

--- gauntlet/packages/gauntlet-serum-multisig/src/lib/constants.ts
+++ gauntlet/packages/gauntlet-serum-multisig/src/lib/constants.ts
@@ -1,5 +1,2 @@
 export const MAX_BUFFER_SIZE = 796
 
-export const MULTISIG_PROGRAM_ID_ENV = 'PROGRAM_ID_MULTISIG'
-export const MULTISIG_NAME = 'serum_multisig'
-export const SCHEMA_PATH = './artifacts/schemas'
