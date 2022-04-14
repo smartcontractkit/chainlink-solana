86d0d22c27c1263ffcff5588aa8b6ced460bd235
Kyle Martin <kylesmartin19@gmail.com>
2022-04-13 21:39:28 -0400

    Get latest transmission event from transaction logs

--- gauntlet/packages/gauntlet-solana-contracts/src/commands/contracts/ocr2/inspection/inspectResponses.ts
+++ gauntlet/packages/gauntlet-solana-contracts/src/commands/contracts/ocr2/inspection/inspectResponses.ts
@@ -40,89 +40,69 @@ export default class OCR2InspectResponses extends SolanaCommand {
   }
 
   /*
-  Gets latest NewTransmission events from latest config block number
-  @param blockNumber block to query
-  @param programId address of ocr2 program
+  Searches for latest transmission event from last {numSigs} transactions
   @param programAccount address of ocr2 program account
+  @param programId address of ocr2 program
   @param idl interface description for ocr2 contract
   @param transmitters list of oracle addresses from on-chain config
+  @param callback function that recieves the NewTransmission
   */
   getLatestTransmissionEvent = async (
-    blockNumber: number,
-    programId: PublicKey,
     programAccount: PublicKey,
+    programId: PublicKey,
     idl: Idl,
     transmitters: PublicKey[],
+    callback: (transmission: NewTransmission) => void,
   ) => {
-    // Event parser callback (used by parseLogs)
-    const logTransmissionEvent = (event: Event) => {
-      // Assemble NewTransmission struct
-      if (event.name == 'NewTransmission') {
-        const transmission: NewTransmission = {
-          roundId: event.data.roundId as number,
-          configDigest: event.data.configDigest as number[],
-          answer: event.data.answer as BN,
-          transmitter: transmitters[event.data.transmitter as number],
-          observationsTimestamp: event.data.observationsTimestamp as number,
-          observerCount: event.data.observerCount as number,
-          observers: (event.data.observers as [])
-            .slice(0, event.data.observerCount as number)
-            .map((observer) => transmitters[observer]),
-          juelsPerLamport: event.data.juelsPerLamport as BN,
-          reimbursementGjuels: event.data.reimbursementGjuels as BN,
-        }
-        // Log transmission data
-        logger.info(`Latest Transmission
-  - Round Id: ${transmission.roundId}
-  - Config Digest: ${transmission.configDigest}
-  - Answer: ${transmission.answer}
-  - Transmitter: ${transmission.transmitter}
-  - Observations Timestamp: ${transmission.observationsTimestamp}
-  - Observer Count: ${transmission.observerCount}
-  - Observers: ${transmission.observers}
-  - Juels Per Lamport: ${transmission.juelsPerLamport}
-  - Reimbursement Gjuels: ${transmission.reimbursementGjuels}
-  `)
-        // Log responding oracle count
-        logger.info(`${transmission.observerCount}/${transmitters.length} oracles are responding
-        `)
-        // Log oracles that are not responsive
-        transmitters.forEach((transmitter) => {
-          // If the transmitter is not listed as an observer, log it
-          if (!transmission.observers.includes(transmitter)) {
-            logger.error(`Oracle ${transmitter} not responding
-            `)
-          }
-        })
-      }
-    }
-
+    // Counts number of NewTransmissions found
+    var newTransmissionCount: number = 0
+    // Tracks the last sig checked
+    var lastSigChecked: string | undefined = undefined
+    // Number of transactions to check at once
+    const batchSize = 100
     // Define coder and event parser
     const coder = new BorshCoder(idl)
     const eventParser = new EventParser(programId, coder)
-    // Get block
-    let block = await this.provider.connection.getBlock(blockNumber)
-    if (!block) {
-      throw new Error('Block not found. Could not find latest block number in config')
-    }
-    // Iterate over all transactions in block
-    block.transactions.forEach((transaction) => {
-      // Get list of accounts keys associated with txn
-      let accountKeys = transaction.transaction.message.accountKeys.map((key) => `${key}`)
-      // Get list of instructions associated with txn
-      let instructions = transaction.transaction.message.instructions
-      // Check each instruction for program id and account
-      instructions.forEach((instruction) => {
-        // Instruction's program id == inputted program id
-        let hasProgramId = accountKeys[instruction.programIdIndex] == programId.toString()
-        // Transaction's account keys contains inputted account key
-        let hasAccount = accountKeys.includes(programAccount.toString())
-        // Parse event logs if above conditions are met
-        if (hasProgramId && hasAccount && transaction.meta && transaction.meta.logMessages) {
-          eventParser.parseLogs(transaction.meta.logMessages, logTransmissionEvent)
+    // Loop until transmission found
+    while (newTransmissionCount == 0) {
+      // Get {batchSize} latest sigs
+      const sigs = await this.provider.connection.getSignaturesForAddress(programAccount, {
+        limit: batchSize,
+        before: lastSigChecked,
+      })
+      // Get the txns associated with the sigs
+      const txns = await this.provider.connection.getParsedTransactions(sigs.map((sig) => sig.signature))
+      txns.forEach((txn) => {
+        // Check txns with logs
+        if (txn && txn.meta && txn.meta.logMessages) {
+          // Parse logs
+          eventParser.parseLogs(txn.meta.logMessages, (event: Event) => {
+            // Only check for NewTransmissions when one has not been found
+            if (event.name != 'NewTransmission' || newTransmissionCount > 0) {
+              return
+            }
+            // Increment new transmission count
+            newTransmissionCount++
+            // Return Transmission using callback
+            callback({
+              roundId: event.data.roundId as number,
+              configDigest: event.data.configDigest as number[],
+              answer: event.data.answer as BN,
+              transmitter: transmitters[event.data.transmitter as number],
+              observationsTimestamp: event.data.observationsTimestamp as number,
+              observerCount: event.data.observerCount as number,
+              observers: (event.data.observers as [])
+                .slice(0, event.data.observerCount as number)
+                .map((observer) => transmitters[observer]),
+              juelsPerLamport: event.data.juelsPerLamport as BN,
+              reimbursementGjuels: event.data.reimbursementGjuels as BN,
+            })
+          })
         }
       })
-    })
+      // Update last sig checked
+      lastSigChecked = sigs[batchSize - 1]
+    }
   }
 
   execute = async () => {
@@ -140,17 +120,42 @@ export default class OCR2InspectResponses extends SolanaCommand {
     - Latest Config Block Number: ${onChainState.config.latestConfigBlockNumber}
     `)
 
-    this.getLatestTransmissionEvent(
-      onChainState.config.latestConfigBlockNumber.toNumber(),
-      ocr2.programId,
-      state,
-      ocr2.idl,
-      onChainState.oracles.xs
-        .map((oracle) => oracle.transmitter)
-        .filter((transmitter) => {
-          return transmitter._bn != 0
-        }),
-    )
+    const transmitters = onChainState.oracles.xs
+      .map((oracle) => oracle.transmitter)
+      .filter((transmitter) => transmitter._bn != 0)
+
+    await this.getLatestTransmissionEvent(state, ocr2.programId, ocr2.idl, transmitters, (event: NewTransmission) => {
+      // Log transmission data
+      logger.info(
+        `Latest Transmission
+    - Round Id: ${event.roundId}
+    - Config Digest: ${event.configDigest}
+    - Answer: ${event.answer}
+    - Transmitter: ${event.transmitter}
+    - Observations Timestamp: ${event.observationsTimestamp}
+    - Observer Count: ${event.observerCount}
+    - Observers: ${event.observers}
+    - Juels Per Lamport: ${event.juelsPerLamport}
+    - Reimbursement Gjuels: ${event.reimbursementGjuels}
+        `,
+      )
+      // Log responding oracle count
+      logger.info(
+        `${event.observerCount}/${transmitters.length} oracles are responding
+        `,
+      )
+      // Log oracles that are not responsive
+      transmitters.forEach((transmitter) => {
+        // If the transmitter is not listed as an observer, log it
+        if (!event.observers.includes(transmitter)) {
+          logger.error(
+            `Oracle ${transmitter} not responding
+            `,
+          )
+        }
+      })
+      return
+    })
 
     const inspections: inspection.Inspection[] = []
 
