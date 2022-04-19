7df1f69f7a248723ee3b1a6b6f9204642fa635ae
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2022-04-19 19:00:52 +0100

    add node operator names to the prometheus metrics

---
+++ pkg/monitoring/node_config.go
@@ -0,0 +1,40 @@
+package monitoring
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+
+	relayMonitoring "github.com/smartcontractkit/chainlink-relay/pkg/monitoring"
+	"github.com/smartcontractkit/libocr/offchainreporting2/types"
+)
+
+type SolanaNodeConfig struct {
+	ID          string   `json:"id,omitempty"`
+	NodeAddress []string `json:"nodeAddress,omitempty"`
+}
+
+func (s SolanaNodeConfig) GetName() string {
+	return s.ID
+}
+
+func (s SolanaNodeConfig) GetAccount() types.Account {
+	address := ""
+	if len(s.NodeAddress) != 0 {
+		address = s.NodeAddress[0]
+	}
+	return types.Account(address)
+}
+
+func SolanaNodesParser(buf io.ReadCloser) ([]relayMonitoring.NodeConfig, error) {
+	rawNodes := []SolanaNodeConfig{}
+	decoder := json.NewDecoder(buf)
+	if err := decoder.Decode(&rawNodes); err != nil {
+		return nil, fmt.Errorf("unable to unmarshal nodes config data: %w", err)
+	}
+	nodes := make([]relayMonitoring.NodeConfig, len(rawNodes))
+	for i, rawNode := range rawNodes {
+		nodes[i] = rawNode
+	}
+	return nodes, nil
+}
