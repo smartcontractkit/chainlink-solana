c48f11aca20838c379e7a7cfb3428514856f6655
gheorghestrimtu <studentcuza@gmail.com>
2022-05-27 15:31:05 +0300

    in progress

--- tests/e2e/utils/ginkgo.go
+++ tests/e2e/utils/ginkgo.go
@@ -2,38 +2,30 @@ package utils
 
 //revive:disable:dot-imports
 import (
-	"os"
-	"path/filepath"
-
-	"github.com/smartcontractkit/chainlink-testing-framework/config"
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 )
 
 // GinkgoSuite provides the default setup for running a Ginkgo test suite
 func GinkgoSuite() {
 	RegisterFailHandler(Fail)
 
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-	fConf, err := config.LoadFrameworkConfig(filepath.Join(TestsDir, "framework.yaml"))
-	if err != nil {
-		log.Fatal().
-			Str("Path", TestsDir).
-			Err(err).
-			Msg("Failed to load config")
-		return
-	}
-	_, err = config.LoadNetworksConfig(filepath.Join(TestsDir, "networks.yaml"))
-	if err != nil {
-		log.Fatal().
-			Str("Path", TestsDir).
-			Err(err).
-			Msg("Failed to load config")
-		return
-	}
-	log.Logger = log.Logger.Level(zerolog.Level(fConf.Logging.Level))
+	//log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	//fConf, err := actions.LoadFrameworkConfig(filepath.Join(TestsDir, "framework.yaml"))
+	//if err != nil {
+	//	log.Fatal().
+	//		Str("Path", TestsDir).
+	//		Err(err).
+	//		Msg("Failed to load config")
+	//	return
+	//}
+	//_, err = config.LoadNetworksConfig(filepath.Join(TestsDir, "networks.yaml"))
+	//if err != nil {
+	//	log.Fatal().
+	//		Str("Path", TestsDir).
+	//		Err(err).
+	//		Msg("Failed to load config")
+	//	return
+	//}
+	////log.Logger = log.Logger.Level(zerolog.Level(fConf.Logging.Level))
 }
