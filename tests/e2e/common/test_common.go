c25844b39101f16fe145e3684e8d3a8f5f318bf4
Blaž Hrastnik <blaz@mxxn.io>
2022-10-28 16:48:15 +0900

    Resolve the validator url

--- tests/e2e/common/test_common.go
+++ tests/e2e/common/test_common.go
@@ -17,6 +17,7 @@ import (
 	"github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/rs/zerolog/log"
+	envclient "github.com/smartcontractkit/chainlink-env/client"
 	"github.com/smartcontractkit/chainlink-env/environment"
 	mockservercfg "github.com/smartcontractkit/chainlink-env/pkg/helm/mockserver-cfg"
 	"github.com/smartcontractkit/chainlink-solana/tests/e2e/solclient"
@@ -225,8 +226,10 @@ func (m *OCRv2TestState) DeployContracts(contractsDir string) {
 	Expect(err).ShouldNot(HaveOccurred())
 	cd.RegisterAnchorPrograms()
 
+	netRemote, err := m.Env.Fwd.FindPort("sol:0", "sol-val", "http-rpc").As(envclient.RemoteConnection, envclient.HTTP)
+	Expect(err).ShouldNot(HaveOccurred())
 	cd.Client.Gauntlet.SetupNetwork(
-		"http://sol:8899",
+		netRemote,
 		cd.Client.ProgramWallets["access_controller-keypair.json"].PublicKey().String(),
 		cd.Client.ProgramWallets["store-keypair.json"].PublicKey().String(),
 		cd.Client.ProgramWallets["ocr2-keypair.json"].PublicKey().String(),
