5f99a6ff888bccd15a4acf584bc7b88b1903ec5c
gheorghestrimtu <studentcuza@gmail.com>
2023-01-05 21:34:07 +0200

    reverse lib.rs changes

--- contracts/programs/access-controller/src/lib.rs
+++ contracts/programs/access-controller/src/lib.rs
@@ -4,7 +4,7 @@ use std::mem;
 
 use arrayvec::arrayvec;
 
-declare_id!("2ckhep7Mvy1dExenBqpcdevhRu7CLuuctMcx7G9mWEvo");
+declare_id!("9xi644bRR8birboDGdTiwBq3C7VEeR7VuamRYYXCubUW");
 
 #[constant]
 pub const MAX_ADDRS: usize = 64;
