59f674a21af3de7ed4209b4b548e100049821425
Tate <tate.exon@smartcontract.com>
2023-01-23 08:48:51 -0700

    Revert "QA-135 Use isomorphic deployments in smoke tests (#451)"
    This reverts commit 1bd89355adc39504f7497d4eb41b7a8fe06ca4df.

--- tests/e2e/smoke/ocr2_test.go
+++ tests/e2e/smoke/ocr2_test.go
@@ -1,11 +1,10 @@
 package smoke
 
 import (
-	"testing"
-	"time"
-
 	"github.com/smartcontractkit/chainlink/integration-tests/actions"
 	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
 
 	"github.com/smartcontractkit/chainlink-solana/tests/e2e/utils"
 
@@ -14,10 +13,7 @@ import (
 
 func TestSolanaOCRV2Smoke(t *testing.T) {
 	var state = common.NewOCRv2State(t, 1, 5)
-	state.DeployCluster(5, false, utils.ContractsDir, 30*time.Minute)
-	if state.Env.WillUseRemoteRunner() {
-		return
-	}
+	state.DeployCluster(5, false, utils.ContractsDir)
 	state.SetAllAdapterResponsesToTheSameValue(10)
 	state.ValidateRoundsAfter(time.Now(), common.NewRoundCheckTimeout, 1)
 	err := actions.TeardownSuite(state.T, state.Env, "logs", state.ChainlinkNodes, nil, nil)
