72f4e799ab058b20630a87611c2b2c57e5817443
Jordan Krage <jmank88@gmail.com>
2023-10-23 08:36:38 -0500

    BCF-2685: move LOOPP command from core (#550)

--- integration-tests/common/test_common.go
+++ integration-tests/common/test_common.go
@@ -197,6 +197,7 @@ func (m *OCRv2TestState) DeployCluster(contractsDir string) {
 			WithMockAdapter().
 			WithCLNodeConfig(m.Common.DefaultNodeConfig()).
 			WithCLNodes(m.Common.NodeCount).
+			WithStandardCleanup().
 			WithTestEnv(env)
 		require.NoError(m.T, err)
 		env, err = b.Build()
---
+++ pkg/solana/cmd/chainlink-solana/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/go-plugin"
+	"github.com/pelletier/go-toml/v2"
+
+	"github.com/smartcontractkit/chainlink-relay/pkg/loop"
+	"github.com/smartcontractkit/chainlink-solana/pkg/solana"
+)
+
+const (
+	loggerName = "PluginSolana"
+)
+
+func main() {
+	s := loop.MustNewStartedServer(loggerName)
+	defer s.Stop()
+
+	p := &pluginRelayer{Plugin: loop.Plugin{Logger: s.Logger}}
+	defer s.Logger.ErrorIfFn(p.Close, "Failed to close")
+
+	s.MustRegister(p)
+
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: loop.PluginRelayerHandshakeConfig(),
+		Plugins: map[string]plugin.Plugin{
+			loop.PluginRelayerName: &loop.GRPCPluginRelayer{
+				PluginServer: p,
+				BrokerConfig: loop.BrokerConfig{
+					StopCh:   stopCh,
+					Logger:   s.Logger,
+					GRPCOpts: s.GRPCOpts,
+				},
+			},
+		},
+		GRPCServer: s.GRPCOpts.NewServer,
+	})
+}
+
+type pluginRelayer struct {
+	loop.Plugin
+}
+
+func (c *pluginRelayer) NewRelayer(ctx context.Context, config string, keystore loop.Keystore) (loop.Relayer, error) {
+	d := toml.NewDecoder(strings.NewReader(config))
+	d.DisallowUnknownFields()
+	var cfg struct {
+		Solana solana.TOMLConfig
+	}
+
+	if err := d.Decode(&cfg); err != nil {
+		return nil, fmt.Errorf("failed to decode config toml: %w:\n\t%s", err, config)
+	}
+
+	opts := solana.ChainOpts{
+		Logger:   c.Logger,
+		KeyStore: keystore,
+	}
+	chain, err := solana.NewChain(&cfg.Solana, opts)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create chain: %w", err)
+	}
+	ra := &loop.RelayerAdapter{Relayer: solana.NewRelayer(c.Logger, chain), RelayerExt: chain}
+
+	c.SubService(ra)
+
+	return ra, nil
+}
