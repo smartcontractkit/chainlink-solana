c4df9bdaa8d83be6ba7ebb772ea80a4e81a9b004
aalu1418 <50029043+aalu1418@users.noreply.github.com>
2024-02-09 12:24:59 -0700

    testing bump e2e test deps

---
+++ scripts/update-e2e.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# get current develop branch sha
+SHA=$(curl https://api.github.com/repos/smartcontractkit/chainlink/commits/develop | jq -r '.sha')
+echo "Chainlink Develop Commit: $SHA"
+
+# update dependencies
+go get github.com/smartcontractkit/chainlink/integration-tests@$SHA
+go get github.com/smartcontractkit/chainlink/v2@$SHA
