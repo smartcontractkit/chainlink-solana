47586b3723fbe7051b49f8f867a13ea3d749ccee
DeividasK <deividas.karzinauskas@gmail.com>
2024-02-13 14:04:16 +0200

    Bump Solana 1.10.33 -> 1.18.1

--- scripts/install-solana-ci.sh
+++ scripts/install-solana-ci.sh
@@ -2,5 +2,5 @@
 
 set -euxo pipefail
 
-sh -c "$(curl -sSfL https://release.solana.com/v1.10.33/install)"
+sh -c "$(curl -sSfL https://release.solana.com/v1.18.1/install)"
 echo "PATH=$HOME/.local/share/solana/install/active_release/bin:$PATH" >> $GITHUB_ENV
