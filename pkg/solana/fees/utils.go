5ef95647bc20739ff908e5c2f1afbe40885985f4
aalu1418 <50029043+aalu1418@users.noreply.github.com>
2024-05-24 14:38:07 -0600

    block history estimator

--- pkg/solana/fees/utils.go
+++ pkg/solana/fees/utils.go
@@ -1,5 +1,12 @@
 package fees
 
+import (
+	"fmt"
+
+	"github.com/gagliardetto/solana-go"
+	"github.com/gagliardetto/solana-go/rpc"
+)
+
 // returns new fee based on number of times bumped
 func CalculateFee(base, max, min uint64, count uint) uint64 {
 	amount := base
@@ -26,4 +33,57 @@ func CalculateFee(base, max, min uint64, count uint) uint64 {
 		return max
 	}
 	return amount
+}
+
+type BlockData struct {
+	Fees   []uint64           // total fee
+	Prices []ComputeUnitPrice // price per unit
+}
+
+// ParseBlock parses the fee calculations from all the transactions within a block
+func ParseBlock(res *rpc.GetBlockResult) (out BlockData, err error) {
+	if res == nil {
+		return out, fmt.Errorf("GetBlockResult was nil")
+	}
+
+	for _, tx := range res.Transactions {
+		if tx.Meta == nil {
+			continue
+		}
+		baseTx, err := tx.GetTransaction()
+		if err != nil {
+			// exit on GetTransaction error
+			// if this occurs, solana-go was unable to parse a transaction
+			// further investigation is required to determine if there is incompatibility
+			return out, fmt.Errorf("failed to GetTransaction (blockhash: %s): %w", res.Blockhash, err)
+		}
+		if baseTx == nil {
+			continue
+		}
+
+		// filter out consensus vote transactions
+		// consensus messages are included as txs within blocks
+		if len(baseTx.Message.Instructions) == 1 &&
+			baseTx.Message.AccountKeys[baseTx.Message.Instructions[0].ProgramIDIndex] == solana.VoteProgramID {
+			continue
+		}
+
+		var price ComputeUnitPrice // default 0
+		for _, instruction := range baseTx.Message.Instructions {
+			// find instructions for compute budget program
+			if baseTx.Message.AccountKeys[instruction.ProgramIDIndex] == solana.MustPublicKeyFromBase58(ComputeBudgetProgram) {
+				parsed, err := ParseComputeUnitPrice(instruction.Data)
+				// if compute unit price found, break instruction loop
+				// only one compute unit price tx is allowed
+				// err returned if not SetComputeUnitPrice instruction
+				if err == nil {
+					price = parsed
+					break
+				}
+			}
+		}
+		out.Prices = append(out.Prices, price)
+		out.Fees = append(out.Fees, tx.Meta.Fee)
+	}
+	return out, err
 }
