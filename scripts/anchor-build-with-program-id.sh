77ef5308f9f33d98e141c0a62f0182a85f8ed555
Jade Park <jonghyeon.park@smartcontract.com>
2024-09-12 14:52:15 +0100

    fix: linux sed support & removed copy stage

--- scripts/anchor-build-with-program-id.sh
+++ scripts/anchor-build-with-program-id.sh
@@ -8,30 +8,35 @@ OCR2_PROGRAM_ID=$3
 
 echo "Current directory: $(pwd)"
 echo "Contents of ./contracts directory:"
-ls ./contracts
 
-WORKSPACE=$(mktemp -d)
-echo "Temporary workspace: $WORKSPACE"
-
-cp -r ./contracts $WORKSPACE/
-
-cd $WORKSPACE/contracts
-
+WORKSPACE=./contracts/
 echo "Workspace contents:"
-ls $WORKSPACE/contracts
+ls $WORKSPACE
 
-# TODO: add linux support
-sed -i '' "s|declare_id!(\"[^\"]*\");|declare_id!(\"$ACCESS_CONTROLLER_PROGRAM_ID\");|" "$WORKSPACE/contracts/programs/access-controller/src/lib.rs"
-sed -i '' "s|declare_id!(\"[^\"]*\");|declare_id!(\"$STORE_PROGRAM_ID\");|" "$WORKSPACE/contracts/programs/store/src/lib.rs"
-sed -i '' "s|declare_id!(\"[^\"]*\");|declare_id!(\"$OCR2_PROGRAM_ID\");|" "$WORKSPACE/contracts/programs/ocr_2/src/lib.rs"
+replaceDeclaredProgramId() {
+  local file="$1"
+  local program_id="$2"
 
-docker run --rm -it -v $(pwd):/workdir backpackapp/build:v0.29.0 /bin/bash -c "anchor build"
+  if [ "$(uname -s)" = "Darwin" ]; then
+    # macOS (Darwin) version
+    sed -i '' "/^declare_id!/s/\"[^\"]*\"/\"$program_id\"/" "$file"
+  else
+    # Linux and other Unix-like systems
+    sed -i "/^declare_id!/s/\"[^\"]*\"/\"$program_id\"/" "$file"
+  fi
+}
 
-echo "Build complete. Artifacts are located in $WORKSPACE/contracts/target/deploy"
-ls $WORKSPACE/contracts/target/deploy
+replaceDeclaredProgramId "$WORKSPACE/programs/access-controller/src/lib.rs" "$ACCESS_CONTROLLER_PROGRAM_ID"
+replaceDeclaredProgramId "$WORKSPACE/programs/store/src/lib.rs" "$STORE_PROGRAM_ID"
+replaceDeclaredProgramId "$WORKSPACE/programs/ocr_2/src/lib.rs" "$OCR2_PROGRAM_ID"
+
+docker run --rm -it -v $WORKSPACE:/workdir backpackapp/build:v0.29.0 /bin/bash -c "anchor build"
+
+echo "Build complete. Artifacts are located in $WORKSPACE/target/deploy"
+ls $WORKSPACE/target/deploy
 
 mkdir -p $WORKSPACE/artifacts/
-cp -r $WORKSPACE/contracts/target/deploy/*.so $WORKSPACE/artifacts/
-cp -r $WORKSPACE/contracts/target/idl/*.json $WORKSPACE/artifacts/
+cp -r $WORKSPACE/target/deploy/*.so $WORKSPACE/artifacts/
+cp -r $WORKSPACE/target/idl/*.json $WORKSPACE/artifacts/
 
 echo "Artifacts and metadata copied to $WORKSPACE/artifacts/"
