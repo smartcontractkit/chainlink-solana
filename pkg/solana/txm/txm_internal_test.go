b5c3891d7ba1baa953ae0c47d92a11ae9b7015c6
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2024-10-02 10:25:25 -0400

    Add multinode to txm

--- pkg/solana/txm/txm_internal_test.go
+++ pkg/solana/txm/txm_internal_test.go
@@ -112,7 +112,7 @@ func TestTxm(t *testing.T) {
 
 			txm := NewTxm(id, func() (client.ReaderWriter, error) {
 				return mc, nil
-			}, cfg, mkey, lggr)
+			}, cfg, nil, mkey, lggr)
 			require.NoError(t, txm.Start(ctx))
 
 			// tracking prom metrics
@@ -718,7 +718,7 @@ func TestTxm_Enqueue(t *testing.T) {
 
 	txm := NewTxm("enqueue_test", func() (client.ReaderWriter, error) {
 		return mc, nil
-	}, cfg, mkey, lggr)
+	}, cfg, nil, mkey, lggr)
 
 	require.ErrorContains(t, txm.Enqueue("txmUnstarted", &solana.Transaction{}), "not started")
 	require.NoError(t, txm.Start(ctx))
