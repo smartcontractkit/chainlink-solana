3802482ce236de717912de88ba3ce72a77a4bf21
Silas Lenihan <sjl@lenihan.net>
2024-10-25 15:09:13 -0400

    Added Address searcher for decoded data

--- pkg/solana/chainwriter/chain_writer.go
+++ pkg/solana/chainwriter/chain_writer.go
@@ -2,16 +2,20 @@ package chainwriter
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"math/big"
+	"reflect"
 
 	"github.com/gagliardetto/solana-go"
 
-	"github.com/smartcontractkit/chainlink-common/pkg/codec"
+	commoncodec "github.com/smartcontractkit/chainlink-common/pkg/codec"
+	"github.com/smartcontractkit/chainlink-common/pkg/codec/encodings/binary"
 	"github.com/smartcontractkit/chainlink-common/pkg/services"
 	"github.com/smartcontractkit/chainlink-common/pkg/types"
 
 	"github.com/smartcontractkit/chainlink-solana/pkg/solana/client"
+	"github.com/smartcontractkit/chainlink-solana/pkg/solana/codec"
 	"github.com/smartcontractkit/chainlink-solana/pkg/solana/fees"
 	"github.com/smartcontractkit/chainlink-solana/pkg/solana/txm"
 )
@@ -20,52 +24,77 @@ type SolanaChainWriterService struct {
 	reader client.Reader
 	txm    txm.Txm
 	ge     fees.Estimator
+	codec  types.Codec
+	config ChainWriterConfig
 }
 
 type ChainWriterConfig struct {
-	Programs map[string]ProgramConfig `json:"contracts" toml:"contracts"`
+	Programs map[string]ProgramConfig
 }
 
 type ProgramConfig struct {
-	Methods map[string]MethodConfig `json:"methods" toml:"methods"`
+	Methods map[string]MethodConfig
+	IDL     string
 }
 
 type MethodConfig struct {
-	InputModifications codec.ModifiersConfig `json:"inputModifications,omitempty"`
-	ChainSpecificName  string                `json:"chainSpecificName"`
+	InputModifications commoncodec.ModifiersConfig
+	EncodedTypeIDL     string
+	DataType           any
+	DecodedTypeName    string
+	ChainSpecificName  string
+	AddressLocations   []string
+	Signers            []solana.AccountMeta
+	Writables          []solana.AccountMeta
 }
 
-func NewSolanaChainWriterService(reader client.Reader, txm txm.Txm, ge fees.Estimator) *SolanaChainWriterService {
+func NewSolanaChainWriterService(reader client.Reader, txm txm.Txm, ge fees.Estimator, config ChainWriterConfig) *SolanaChainWriterService {
 	return &SolanaChainWriterService{
 		reader: reader,
 		txm:    txm,
 		ge:     ge,
+		config: config,
 	}
 }
 
-var (
-	_ services.Service  = &SolanaChainWriterService{}
-	_ types.ChainWriter = &SolanaChainWriterService{}
-)
-
 func (s *SolanaChainWriterService) SubmitTransaction(ctx context.Context, contractName, method string, args any, transactionID string, toAddress string, meta *types.TxMeta, value *big.Int) error {
+	programConfig := s.config.Programs[contractName]
+	methodConfig := programConfig.Methods[method]
+
 	data, ok := args.([]byte)
 	if !ok {
 		return fmt.Errorf("Unable to convert args to []byte")
 	}
 
-	blockhash, err := s.reader.LatestBlockhash()
+	// decode data
+	var idl codec.IDL
+	err := json.Unmarshal([]byte(methodConfig.EncodedTypeIDL), &idl)
+	if err != nil {
+		return fmt.Errorf("error unmarshalling IDL: %w", err)
+	}
+	cwCodec, err := codec.NewIDLAccountCodec(idl, binary.LittleEndian())
+	if err != nil {
+		return fmt.Errorf("error creating new IDLAccountCodec: %w", err)
+	}
+
+	// Create an instance of the type defined by methodConfig.DataType
+	decoded := reflect.New(reflect.TypeOf(methodConfig.DataType)).Interface()
+	err = cwCodec.Decode(ctx, data, decoded, methodConfig.DecodedTypeName)
+
+	accounts, err := GetAddressesFromDecodedData(decoded, methodConfig.AddressLocations)
+	if err != nil {
+		return fmt.Errorf("error getting addresses from decoded data: %w", err)
+	}
+
+	blockhash, err := s.reader.LatestBlockhash(ctx)
 
 	programId, err := solana.PublicKeyFromBase58(contractName)
 	if err != nil {
 		return fmt.Errorf("Error getting programId: %w", err)
 	}
 
-	// placeholder method to get accounts
-	accounts, feePayer, err := getAccounts(contractName, method, args)
-	if err != nil || len(accounts) == 0 {
-		return fmt.Errorf("Error getting accounts: %w", err)
-	}
+	// This isn't a real method, TBD how we will get this
+	feePayer := accounts[0]
 
 	tx, err := solana.NewTransaction(
 		[]solana.Instruction{
@@ -78,11 +107,16 @@ func (s *SolanaChainWriterService) SubmitTransaction(ctx context.Context, contra
 		return fmt.Errorf("error creating new transaction: %w", err)
 	}
 
-	if err = s.txm.Enqueue(accounts[0].PublicKey.String(), tx); err != nil {
+	if err = s.txm.Enqueue(ctx, accounts[0].PublicKey.String(), tx); err != nil {
 		return fmt.Errorf("error on sending trasnaction to TXM: %w", err)
 	}
 	return nil
 }
+
+var (
+	_ services.Service  = &SolanaChainWriterService{}
+	_ types.ChainWriter = &SolanaChainWriterService{}
+)
 
 func getAccounts(contractName string, method string, args any) (accounts []*solana.AccountMeta, feePayer *solana.AccountMeta, err error) {
 	// TO DO: Use on-chain team's helper functions to get the accounts from CCIP related metadata.
