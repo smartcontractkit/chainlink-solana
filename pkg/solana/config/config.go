0c0f113fbc7d578ebcaa8427e654cfdf7aba4245
Farber98 <juan.farber@smartcontract.com>
2024-11-01 17:47:54 -0300

    keep blockHistorySize near BlockHistoryPollPeriod

--- pkg/solana/config/config.go
+++ pkg/solana/config/config.go
@@ -29,9 +29,9 @@ var defaultConfigSet = Chain{
 	ComputeUnitPriceDefault:  ptr(uint64(0)),
 	FeeBumpPeriod:            config.MustNewDuration(3 * time.Second), // set to 0 to disable fee bumping
 	BlockHistoryPollPeriod:   config.MustNewDuration(5 * time.Second),
+	BlockHistorySize:         ptr(uint64(1)),       // 1: uses latest block; >1: Uses multiple blocks, where n is number of blocks. DISCLAIMER: 1:1 ratio between n and RPC calls.
 	ComputeUnitLimitDefault:  ptr(uint32(200_000)), // set to 0 to disable adding compute unit limit
 	EstimateComputeUnitLimit: ptr(false),           // set to false to disable compute unit limit estimation
-	BlockHistorySize:         ptr(uint64(1)),       // 1: LatestBlockEstimator; >1: MultipleBlocksEstimator where n is number of blocks.
 }
 
 //go:generate mockery --name Config --output ./mocks/ --case=underscore --filename config.go
@@ -54,9 +54,9 @@ type Config interface {
 	ComputeUnitPriceDefault() uint64
 	FeeBumpPeriod() time.Duration
 	BlockHistoryPollPeriod() time.Duration
+	BlockHistorySize() uint64
 	ComputeUnitLimitDefault() uint32
 	EstimateComputeUnitLimit() bool
-	BlockHistorySize() uint64
 }
 
 type Chain struct {
@@ -76,9 +76,9 @@ type Chain struct {
 	ComputeUnitPriceDefault  *uint64
 	FeeBumpPeriod            *config.Duration
 	BlockHistoryPollPeriod   *config.Duration
+	BlockHistorySize         *uint64
 	ComputeUnitLimitDefault  *uint32
 	EstimateComputeUnitLimit *bool
-	BlockHistorySize         *uint64
 }
 
 func (c *Chain) SetDefaults() {
@@ -130,14 +130,14 @@ func (c *Chain) SetDefaults() {
 	if c.BlockHistoryPollPeriod == nil {
 		c.BlockHistoryPollPeriod = defaultConfigSet.BlockHistoryPollPeriod
 	}
+	if c.BlockHistorySize == nil {
+		c.BlockHistorySize = defaultConfigSet.BlockHistorySize
+	}
 	if c.ComputeUnitLimitDefault == nil {
 		c.ComputeUnitLimitDefault = defaultConfigSet.ComputeUnitLimitDefault
 	}
 	if c.EstimateComputeUnitLimit == nil {
 		c.EstimateComputeUnitLimit = defaultConfigSet.EstimateComputeUnitLimit
-	}
-	if c.BlockHistorySize == nil {
-		c.BlockHistorySize = defaultConfigSet.BlockHistorySize
 	}
 }
 
