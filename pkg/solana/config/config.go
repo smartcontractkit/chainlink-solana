15a3a13a2bf035cd2f396088f99d98dc48481257
amit-momin <amit.momin@smartcontract.com>
2024-11-04 10:22:33 -0600

    Set retention period to 1 minute

--- pkg/solana/config/config.go
+++ pkg/solana/config/config.go
@@ -18,7 +18,7 @@ var defaultConfigSet = Chain{
 	TxTimeout:           config.MustNewDuration(time.Minute),            // timeout for send tx method in client
 	TxRetryTimeout:      config.MustNewDuration(10 * time.Second),       // duration for tx rebroadcasting to RPC node
 	TxConfirmTimeout:    config.MustNewDuration(30 * time.Second),       // duration before discarding tx as unconfirmed. Set to 0 to disable discarding tx.
-	TxRetentionTimeout:  config.MustNewDuration(0 * time.Second),        // duration to retain transactions after being marked as finalized or errored
+	TxRetentionTimeout:  config.MustNewDuration(1 * time.Minute),        // duration to retain transactions after being marked as finalized or errored
 	SkipPreflight:       ptr(true),                                      // to enable or disable preflight checks
 	Commitment:          ptr(string(rpc.CommitmentConfirmed)),
 	MaxRetries:          ptr(int64(0)), // max number of retries (default = 0). when config.MaxRetries < 0), interpreted as MaxRetries = nil and rpc node will do a reasonable number of retries
