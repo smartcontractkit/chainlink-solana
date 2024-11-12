aa9279c5b002a25f9601f2ff9776732271411e90
Farber98 <juan.farber@smartcontract.com>
2024-11-12 07:46:10 -0300

    fix config typo

--- pkg/solana/chain.go
+++ pkg/solana/chain.go
@@ -580,7 +580,7 @@ func (c *chain) sendTx(ctx context.Context, from, to string, amount *big.Int, ba
 		txm.SetComputeUnitPriceMax(0),
 		txm.SetComputeUnitPriceMin(0),
 		txm.SetBaseComputeUnitPrice(0),
-		txm.SetFeeBumpCriteria("fixed-interval"),
+		txm.SetFeeBumpCriteria("fixed-intervals"),
 		txm.SetFeeBumpPeriod(0),
 	)
 	if err != nil {
