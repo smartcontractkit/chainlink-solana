dd3e14ce5b4732c1802bb36e9b00cbd947eae5ab
amit-momin <amit.momin@smartcontract.com>
2024-11-21 15:12:24 -0600

    Updated default configs for soak test

--- pkg/solana/config/config.go
+++ pkg/solana/config/config.go
@@ -18,7 +18,7 @@ var defaultConfigSet = Chain{
 	TxTimeout:           config.MustNewDuration(time.Minute),            // timeout for send tx method in client
 	TxRetryTimeout:      config.MustNewDuration(10 * time.Second),       // duration for tx rebroadcasting to RPC node
 	TxConfirmTimeout:    config.MustNewDuration(30 * time.Second),       // duration before discarding tx as unconfirmed. Set to 0 to disable discarding tx.
-	TxRetentionTimeout:  config.MustNewDuration(0 * time.Second),        // duration to retain transactions after being marked as finalized or errored. Set to 0 to immediately drop transactions.
+	TxRetentionTimeout:  config.MustNewDuration(8 * time.Hour),        // duration to retain transactions after being marked as finalized or errored. Set to 0 to immediately drop transactions.
 	SkipPreflight:       ptr(true),                                      // to enable or disable preflight checks
 	Commitment:          ptr(string(rpc.CommitmentConfirmed)),
 	MaxRetries:          ptr(int64(0)), // max number of retries (default = 0). when config.MaxRetries < 0), interpreted as MaxRetries = nil and rpc node will do a reasonable number of retries
@@ -32,7 +32,7 @@ var defaultConfigSet = Chain{
 	BlockHistoryPollPeriod:   config.MustNewDuration(5 * time.Second),
 	BlockHistorySize:         ptr(uint64(1)),       // 1: uses latest block; >1: Uses multiple blocks, where n is number of blocks. DISCLAIMER: 1:1 ratio between n and RPC calls.
 	ComputeUnitLimitDefault:  ptr(uint32(200_000)), // set to 0 to disable adding compute unit limit
-	EstimateComputeUnitLimit: ptr(false),           // set to false to disable compute unit limit estimation
+	EstimateComputeUnitLimit: ptr(true),           // set to false to disable compute unit limit estimation
 }
 
 type Config interface {
