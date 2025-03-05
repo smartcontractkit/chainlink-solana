9aad561981093c91c43a1697c1bcd2baa423ee1e
Blaž Hrastnik <blaz@mxxn.io>
2025-03-06 03:41:32 +0900

    Correctly set tokenIndexes and isWritable according to TokenAdminRegistry

--- pkg/solana/chainwriter/transform_registry.go
+++ pkg/solana/chainwriter/transform_registry.go
@@ -5,10 +5,15 @@ import (
 	"errors"
 	"fmt"
 
+	bin "github.com/gagliardetto/binary"
 	"github.com/gagliardetto/solana-go"
+	"github.com/gagliardetto/solana-go/rpc"
 	"github.com/mitchellh/mapstructure"
+
 	"github.com/smartcontractkit/chainlink-ccip/chains/solana/gobindings/ccip_offramp"
+	"github.com/smartcontractkit/chainlink-ccip/chains/solana/gobindings/ccip_router"
 	"github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
+	"github.com/smartcontractkit/chainlink-solana/pkg/solana/client"
 )
 
 type ReportPostTransform struct {
@@ -19,7 +24,7 @@ type ReportPostTransform struct {
 	TokenIndexes   []byte
 }
 
-func FindTransform(id string) (func(context.Context, any, solana.AccountMetaSlice, map[string]map[string][]*solana.AccountMeta) (any, solana.AccountMetaSlice, error), error) {
+func FindTransform(id string) (func(context.Context, client.MultiClient, any, solana.AccountMetaSlice, map[string]map[string][]*solana.AccountMeta) (any, solana.AccountMetaSlice, error), error) {
 	switch id {
 	case "CCIPExecute":
 		return CCIPExecuteArgsTransform, nil
@@ -32,14 +37,14 @@ func FindTransform(id string) (func(context.Context, any, solana.AccountMetaSlic
 
 // This Transform function looks up the token pool addresses in the accounts slice and augments the args
 // with the indexes of the token pool addresses in the accounts slice.
-func CCIPExecuteArgsTransform(ctx context.Context, args any, accounts solana.AccountMetaSlice, tableMap map[string]map[string][]*solana.AccountMeta) (any, solana.AccountMetaSlice, error) {
+func CCIPExecuteArgsTransform(ctx context.Context, client client.MultiClient, args any, accounts solana.AccountMetaSlice, tableMap map[string]map[string][]*solana.AccountMeta) (any, solana.AccountMetaSlice, error) {
 	var argsTransformed ReportPostTransform
 	err := mapstructure.Decode(args, &argsTransformed)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	registryTables, exists := tableMap["PoolLookupTable"]
+	poolTables, exists := tableMap["PoolLookupTable"]
 	// If PoolLookupTable does not exist in the table map, token indexes are not needed
 	// Return with empty TokenIndexes
 	if !exists {
@@ -47,24 +52,44 @@ func CCIPExecuteArgsTransform(ctx context.Context, args any, accounts solana.Acc
 		return argsTransformed, accounts, nil
 	}
 
-	tokenPoolAddresses := []solana.PublicKey{}
-	for _, table := range registryTables {
-		tokenPoolAddresses = append(tokenPoolAddresses, table[0].PublicKey)
-	}
+	// TODO: needs offsetting for receiver_program, account, etc. at the start of remaining accounts
+	offset := 0
+	tokenIndexes := make([]uint8, 0, len(poolTables))
+	for _, table := range poolTables {
+		tokenAdminRegistryPDA := table[1].PublicKey
 
-	tokenIndexes := []uint8{}
-	for i, account := range accounts {
-		for _, address := range tokenPoolAddresses {
-			if account.PublicKey == address {
-				if i > 255 {
-					return nil, nil, fmt.Errorf("index %d out of range for uint8", i)
-				}
-				tokenIndexes = append(tokenIndexes, uint8(i)) //nolint:gosec
-			}
+		// load token admin registry
+		resp, err := client.GetAccountInfoWithOpts(ctx, tokenAdminRegistryPDA, &rpc.GetAccountInfoOpts{
+			Encoding:   "base64",
+			Commitment: rpc.CommitmentFinalized,
+		})
+		if err != nil {
+			return nil, nil, err
 		}
+		tokenAdminRegistry := ccip_router.TokenAdminRegistry{}
+		bin.NewBorshDecoder(resp.GetBinary()).Decode(&tokenAdminRegistry)
+
+		// copied from utils/tokens/tokenpool.go
+		writableBytes := append(tokenAdminRegistry.WritableIndexes[0].Bytes(), tokenAdminRegistry.WritableIndexes[1].Bytes()...)
+		writableBits := ""
+		for _, b := range writableBytes {
+			writableBits += fmt.Sprintf("%08b", b)
+		}
+
+		// set is_writable according to token admin registry
+		for i := range table {
+			writable := string(writableBits[i]) == "1"
+			// skip first 11 accounts, since they are mandatory/not remaining_accounts
+			// skip 3 accounts since they indicate other accounts than lookup field
+			accounts[11+offset+3+i].IsWritable = writable
+		}
+
+		// calculate the token index
+		tokenIndexes = append(tokenIndexes, uint8(offset)) //nolint:gosec
+		offset += len(table) + 3
 	}
 
-	if len(tokenIndexes) != len(tokenPoolAddresses) {
+	if len(tokenIndexes) != len(poolTables) {
 		return nil, nil, fmt.Errorf("missing token pools in accounts")
 	}
 
@@ -73,7 +98,7 @@ func CCIPExecuteArgsTransform(ctx context.Context, args any, accounts solana.Acc
 }
 
 // This Transform function trims off the GlobalState account from commit transactions if there are no token or gas price updates
-func CCIPCommitAccountTransform(ctx context.Context, args any, accounts solana.AccountMetaSlice, _ map[string]map[string][]*solana.AccountMeta) (any, solana.AccountMetaSlice, error) {
+func CCIPCommitAccountTransform(ctx context.Context, client client.MultiClient, args any, accounts solana.AccountMetaSlice, _ map[string]map[string][]*solana.AccountMeta) (any, solana.AccountMetaSlice, error) {
 	var tokenPriceVals, gasPriceVals [][]byte
 	var err error
 	tokenPriceVals, err = GetValuesAtLocation(args, "Info.TokenPrices.TokenID")
