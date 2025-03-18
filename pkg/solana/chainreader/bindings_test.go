4a8d6aaef183808c599483c5666797b72bc3d042
amit-momin <amit.momin@smartcontract.com>
2025-03-18 14:42:05 -0500

    Linted

--- pkg/solana/chainreader/bindings_test.go
+++ pkg/solana/chainreader/bindings_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/smartcontractkit/chainlink-common/pkg/types"
 	"github.com/smartcontractkit/chainlink-common/pkg/types/query"
 	"github.com/smartcontractkit/chainlink-common/pkg/utils/tests"
+
 	"github.com/smartcontractkit/chainlink-solana/pkg/solana/codec"
 	"github.com/smartcontractkit/chainlink-solana/pkg/solana/config"
 )
