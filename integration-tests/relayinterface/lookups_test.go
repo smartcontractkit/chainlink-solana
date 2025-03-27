9a8ec92c051b48888ae700e645261ab2dc23af55
Dylan Tinianov <dylan.tinianov@smartcontract.com>
2025-03-27 10:46:33 -0400

    Fix import

--- integration-tests/relayinterface/lookups_test.go
+++ integration-tests/relayinterface/lookups_test.go
@@ -8,10 +8,10 @@ import (
 
 	"github.com/gagliardetto/solana-go"
 	"github.com/gagliardetto/solana-go/rpc"
-	"github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
 	"github.com/stretchr/testify/require"
 
 	"github.com/smartcontractkit/chainlink-ccip/chains/solana/utils/tokens"
+	"github.com/smartcontractkit/chainlink-ccip/pkg/types/ccipocr3"
 	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"github.com/smartcontractkit/chainlink-solana/integration-tests/utils"
 
