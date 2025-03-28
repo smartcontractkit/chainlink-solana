389ec0ecf359b3e20cc88b14bdfff6a722def29d
Joe Huang <joe.huang@smartcontract.com>
2025-03-28 11:34:17 -0500

    revert

--- integration-tests/smoke/ocr2_test.go
+++ integration-tests/smoke/ocr2_test.go
@@ -10,8 +10,6 @@ import (
 	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/require"
 
-	"github.com/smartcontractkit/chainlink-solana/pkg/solana/client"
-
 	"github.com/smartcontractkit/chainlink/integration-tests/docker/test_env"
 
 	"github.com/smartcontractkit/chainlink-solana/integration-tests/common"
@@ -102,7 +100,7 @@ func startOCR2DataFeedsSmokeTest(t *testing.T, testname string, testenv map[stri
 	err = sg.InstallDependencies()
 	require.NoError(t, err, "Error installing gauntlet dependencies")
 
-	if *config.Common.Network == client.DevnetGenesisHash {
+	if *config.Common.Network == "devnet" {
 		state.Common.ChainDetails.ProgramAddresses.OCR2 = *config.SolanaConfig.OCR2ProgramID
 		state.Common.ChainDetails.ProgramAddresses.AccessController = *config.SolanaConfig.AccessControllerProgramID
 		state.Common.ChainDetails.ProgramAddresses.Store = *config.SolanaConfig.StoreProgramID
