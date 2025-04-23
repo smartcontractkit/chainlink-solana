d7ac12baa35d4c7ab50ae42f6b39f44dc9133076
Augustus Chang <augustus.chang@smartcontract.com>
2025-04-23 17:24:26 -0400

    fix hyphen

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -11,8 +11,8 @@ function modify_program {
   sed -i "s/9xi644bRR8birboDGdTiwBq3C7VEeR7VuamRYYXCubUW/$ac/" "${BASH_SOURCE%/*}/../contracts/programs/access-controller/src/lib.rs"
   sed -i "s/HEvSKofvBgfaexv23kMabbYqxasxU3mQ4ibBMEmJWHny/$store/" "${BASH_SOURCE%/*}/../contracts/programs/store/src/lib.rs"
   sed -i "s/cjg3oHmg9uuPsP8D6g29NWvhySJkdYdAo9D25PRbKXJ/$ocr2/" "${BASH_SOURCE%/*}/../contracts/programs/ocr_2/src/lib.rs"
-  sed -i "s/whV7Q5pi17hPPyaPksToDw1nMx6Lh8qmNWKFaLRQ4wz/$keystone_forwarder/" "${BASH_SOURCE%/*}/../contracts/programs/keystone_forwarder/src/lib.rs"
-  sed -i "s/5z38tFCAmcPJb1DXUHSoKQhR8qQ8o9aNZ8rZFWe6gH4L/$dummy_receiver/" "${BASH_SOURCE%/*}/../contracts/programs/dummy_receiver/src/lib.rs"
+  sed -i "s/whV7Q5pi17hPPyaPksToDw1nMx6Lh8qmNWKFaLRQ4wz/$keystone_forwarder/" "${BASH_SOURCE%/*}/../contracts/programs/keystone-forwarder/src/lib.rs"
+  sed -i "s/5z38tFCAmcPJb1DXUHSoKQhR8qQ8o9aNZ8rZFWe6gH4L/$dummy_receiver/" "${BASH_SOURCE%/*}/../contracts/programs/dummy-receiver/src/lib.rs"
 
 }
 
