5d9e7d661dd1c6ece14402fafd7eebcc3b873abd
Dmytro Haidashenko <dmytro.haidashenko@smartcontract.com>
2025-04-24 14:07:48 +0200

    Try to reduce flakyness in solana account funding

--- pkg/solana/chain_test.go
+++ pkg/solana/chain_test.go
@@ -281,7 +281,7 @@ func TestChain_Transact(t *testing.T) {
 	require.NoError(t, err)
 	receiver, err := solana.NewRandomPrivateKey()
 	require.NoError(t, err)
-	amount := big.NewInt(100_000_000_000 - 5_000) // total balance - tx fee
+	amount := big.NewInt(100_000_000_000 - 50_000) // total balance - tx fee with a buffer
 	err = client.FundTestAccountsWithError(t, solana.PublicKeySlice{sender.PublicKey()}, url)
 	require.NoError(t, err)
 
