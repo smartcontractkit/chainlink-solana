1d67c25383dc3024b8723ce0de4832c228c983d0
amit-momin <amit.momin@smartcontract.com>
2025-05-13 17:43:56 -0500

    Added sanity nil check

--- pkg/solana/txm/txm.go
+++ pkg/solana/txm/txm.go
@@ -78,6 +78,9 @@ func NewTxm(chainID string, client utils.Loader[client.ReaderWriter],
 	if sendTx == nil {
 		// default sendTx using a single RPC
 		sendTx = func(ctx context.Context, tx *solanaGo.Transaction) (solanaGo.Signature, error) {
+			if tx == nil {
+				return solanaGo.Signature{}, fmt.Errorf("transaction is nil")
+			}
 			c, err := client.Get(ctx)
 			if err != nil {
 				return solanaGo.Signature{}, err
