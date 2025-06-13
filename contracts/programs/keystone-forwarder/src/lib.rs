aeb11864f2ea1838107eddc283703465795aaad4
chray-zhang <chray.zhang@smartcontract.com>
2025-06-13 10:46:02 -0400

    Zero OracleConfig (#1263)
    * Zero OracleConfig
    
    * Formatting
    
    * revision
    
    * contract changes
    
    * Fixes
    
    * revision
    
    ---------
    
    Co-authored-by: Augustus <14297860+augustbleeds@users.noreply.github.com>

--- contracts/programs/keystone-forwarder/src/lib.rs
+++ contracts/programs/keystone-forwarder/src/lib.rs
@@ -84,7 +84,7 @@ pub mod keystone_forwarder {
         f: u8,
         signer_addresses: Vec<[u8; 20]>,
     ) -> Result<()> {
-        let config = &mut ctx.accounts.oracles_config;
+        let config = &mut ctx.accounts.oracles_config.load_init()?;
 
         set_oracles_config(config, don_id, config_version, f, signer_addresses)
     }
@@ -96,7 +96,7 @@ pub mod keystone_forwarder {
         f: u8,
         signer_addresses: Vec<[u8; 20]>,
     ) -> Result<()> {
-        let config = &mut ctx.accounts.oracles_config;
+        let config = &mut ctx.accounts.oracles_config.load_mut()?;
 
         set_oracles_config(config, don_id, config_version, f, signer_addresses)
     }
@@ -120,7 +120,7 @@ pub mod keystone_forwarder {
         require_gt!(data.len(), min_data_size, ForwarderError::InvalidReport);
 
         // get config
-        let oracles_config = &ctx.accounts.oracles_config;
+        let oracles_config = ctx.accounts.oracles_config.load()?;
         let f = oracles_config.f;
         require_neq!(f, 0, ForwarderError::InvalidConfig);
 
@@ -139,7 +139,7 @@ pub mod keystone_forwarder {
         let data = &data[total_signature_len..];
         let hashed_report = hash::hash(data).to_bytes();
 
-        verify_signatures(&hashed_report, signatures, oracles_config, num_signatures)?;
+        verify_signatures(&hashed_report, signatures, &oracles_config, num_signatures)?;
 
         // slice raw_report from the report context
         let raw_report_end = data.len() - REPORT_CONTEXT_LEN;
@@ -226,7 +226,7 @@ pub mod keystone_forwarder {
 fn verify_signatures(
     hashed_report: &[u8; 32],
     signatures: &[u8],
-    oracles_config: &Account<OraclesConfig>,
+    oracles_config: &OraclesConfig,
     num_signers: usize,
 ) -> Result<()> {
     // ensure MAX_SIGNERS fit in the bits of uniques
@@ -246,6 +246,7 @@ fn verify_signatures(
 
         let index = oracles_config
             .signer_addresses
+            .as_slice()
             .binary_search_by(|addr| addr.cmp(&signer_eth_address))
             .map_err(|_| ForwarderError::UnauthorizedSigner)?;
 
@@ -262,7 +263,7 @@ fn verify_signatures(
 }
 
 fn set_oracles_config(
-    oracles_config: &mut Account<OraclesConfig>,
+    oracles_config: &mut OraclesConfig,
     don_id: u32,
     config_version: u32,
     f: u8,
@@ -294,7 +295,9 @@ fn set_oracles_config(
 
     oracles_config.config_id = get_config_id(don_id, config_version);
     oracles_config.f = f;
-    oracles_config.signer_addresses = signer_addresses.clone();
+    oracles_config.signer_addresses.clear();
+
+    oracles_config.signer_addresses.extend(&signer_addresses);
 
     emit!(ConfigSet {
         don_id,
