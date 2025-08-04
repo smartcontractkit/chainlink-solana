02018e687bcdab58020886c5cce7ca1645e0bd23
Austin <107539019+0xAustinWang@users.noreply.github.com>
2025-08-04 12:37:34 -1000

    bump txm simulations from debug to error (#1314)

--- pkg/solana/txm/txm.go
+++ pkg/solana/txm/txm.go
@@ -670,7 +670,7 @@ func (txm *Txm) simulate() {
 			// Certain errors can be considered not to be failures during simulation to allow the process to continue
 			if txState, errType := txm.ProcessError(ctx, msg.signatures[0], res.Err, true, msg.id); errType != NoFailure {
 				if len(res.Logs) > 0 {
-					txm.lggr.Debugw("simulated transaction error logs", "logs", res.Logs)
+					txm.lggr.Errorw("simulated transaction error logs", "logs", res.Logs)
 				}
 				id, err := txm.txs.OnError(msg.signatures[0], txm.cfg.TxRetentionTimeout(), txState, errType)
 				if err != nil {
@@ -987,7 +987,7 @@ func (txm *Txm) EstimateComputeUnitLimit(ctx context.Context, tx *solanaGo.Trans
 		// Certain errors can be considered not to be failures during simulation to allow the process to continue
 		if txState, errType := txm.ProcessError(ctx, sig, res.Err, true, id); errType != NoFailure {
 			if len(res.Logs) > 0 {
-				txm.lggr.Debugw("simulated transaction error logs", "logs", res.Logs)
+				txm.lggr.Errorw("simulated transaction error logs", "logs", res.Logs)
 			}
 			err := txm.txs.OnPrebroadcastError(id, txm.cfg.TxRetentionTimeout(), txState, errType)
 			if err != nil {
