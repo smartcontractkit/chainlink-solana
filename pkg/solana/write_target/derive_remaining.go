cbbeb22b71f2961e24cc9f781207587f1fcb1c20
Vladimir Shchukin <vladimir.shchukin@smartcontract.com>
2025-08-05 12:24:21 -0400

    add err

--- pkg/solana/write_target/derive_remaining.go
+++ pkg/solana/write_target/derive_remaining.go
@@ -203,7 +203,8 @@ func (dr *deriver) deriveRemaining(ctx context.Context, cd *CacheDetails, meta c
 
 		writeFlagAccount, err := dr.client.GetAccountInfoWithOpts(ctx, writeFlagKey, &rpc.GetAccountInfoOpts{Commitment: rpc.CommitmentProcessed})
 		if err != nil {
-			return nil, fmt.Errorf("error fetching write flag account %v for data id %v  repHash: %x authority: %s cacheState: %s cacheProgram: %s", writeFlagKey, feedID, reportHash, authority.String(), cacheStateKey.String(), cacheProgram.String())
+			return nil, fmt.Errorf("error fetching write flag account %v for data id %v owner: %x ID: %x data: %x", writeFlagKey, feedID,
+				[]byte(meta.WorkflowOwner), []byte(meta.WorkflowID), dataID.Bytes())
 		}
 
 		if writeFlagAccount.Value == nil {
