50f8f67eb21eb4ba91f47ab06d53b05e6c593350
amit-momin <108959691+amit-momin@users.noreply.github.com>
2025-08-07 10:02:37 -0500

    Reverted TXM log level (#1315)

--- pkg/solana/txm/txm.go
+++ pkg/solana/txm/txm.go
@@ -670,7 +670,9 @@ func (txm *Txm) simulate() {
 			// Certain errors can be considered not to be failures during simulation to allow the process to continue
 			if txState, errType := txm.ProcessError(ctx, msg.signatures[0], res.Err, true, msg.id); errType != NoFailure {
 				if len(res.Logs) > 0 {
-					txm.lggr.Errorw("simulated transaction error logs", "logs", res.Logs)
+					// Although this surfaces revert errors, keep log level as Debug. Data Feeds expects stale report reverts often.
+					// Increasing the level to Error would generate too much noise.
+					txm.lggr.Debugw("simulated transaction error logs", "logs", res.Logs)
 				}
 				id, err := txm.txs.OnError(msg.signatures[0], txm.cfg.TxRetentionTimeout(), txState, errType)
 				if err != nil {
