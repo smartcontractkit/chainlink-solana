632bebc0e8024559586dde1b0ef2577027ac28bc
Ryan Tinianov <tinianov@live.com>
2025-08-20 09:53:04 -0400

    Update to use the values package from chainlink-proto (#1326)
    * Update to use the values package from chainlink-proto
    
    * update common
    
    
    
    ---------
    
    Co-authored-by: Vladimir <vladimir.shchukin@smartcontract.com>

--- pkg/solana/chainreader/chain_reader.go
+++ pkg/solana/chainreader/chain_reader.go
@@ -23,7 +23,7 @@ import (
 	"github.com/smartcontractkit/chainlink-common/pkg/types"
 	"github.com/smartcontractkit/chainlink-common/pkg/types/query"
 	"github.com/smartcontractkit/chainlink-common/pkg/types/query/primitives"
-	"github.com/smartcontractkit/chainlink-common/pkg/values"
+	"github.com/smartcontractkit/chainlink-protos/cre/go/values"
 
 	"github.com/smartcontractkit/chainlink-solana/pkg/solana/codec"
 	"github.com/smartcontractkit/chainlink-solana/pkg/solana/config"
