91d25f2f0ebc0f776a06c2c059012e2d2ffb1003
Silas Lenihan <sjl@lenihan.net>
2026-01-29 09:42:06 -0500

    Revert "Renamed eventIDLJson"
    This reverts commit 43e8e7fafa96e9ff639cdd43e02d22e3152a355c.

--- pkg/solana/solana_service.go
+++ pkg/solana/solana_service.go
@@ -636,7 +636,7 @@ func convertSolPubKeysToCommon(keys []solana.PublicKey) []commonsol.PublicKey {
 
 func convertFilter(f commonsol.LPFilterQuery) (logpollertypes.Filter, error) {
 	var idl logpollertypes.EventIdl
-	err := json.Unmarshal(f.ContractIdlJSON, &idl)
+	err := json.Unmarshal(f.EventIdlJSON, &idl)
 	if err != nil {
 		return logpollertypes.Filter{}, fmt.Errorf("invalid event idl: %w", err)
 	}
